0211ac8c185adac9c46cb8f0105ab7eac63a00c4
Matt <1009003+tantaman@users.noreply.github.com>
2022-07-27 17:14:26 -0400

    get index list for a table so we can process that too

--- prototype/migrator/src/migrateTableSchema.ts
+++ prototype/migrator/src/migrateTableSchema.ts
@@ -1,13 +1,23 @@
 import { Database as DB } from "better-sqlite3";
 import chalk from "chalk";
 
-type TableInfo = {
+type ColumnInfo = {
   cid: number; // column id (order)
   name: string; // column name
   type: string; // data type (if any)
   notnull: number; // 0 no, 1 yes
   dflt_value: any; // default value for the column
   pk: number; // primary key. 0 no, 1 yes
+};
+type TableInfo = ColumnInfo[];
+
+// https://www.sqlite.org/pragma.html#pragma_index_info
+type IndexInfo = {
+  seq: number;
+  name: string;
+  unique: number;
+  origin: string; // c, pk, u
+  partial: number; // https://www.sqlite.org/partialindex.html
 };
 
 export default function migrateTableSchemas(
@@ -16,13 +26,31 @@ export default function migrateTableSchemas(
   which: string[]
 ) {
   which.forEach((w) =>
-    createCrrSchemasFor(dest, w, src.pragma(`table_info(${w})`))
+    createCrrSchemasFor(
+      dest,
+      w,
+      src.pragma(`table_info(${w})`),
+      src.pragma(`index_list(${w})`)
+    )
   );
 }
 
-function createCrrSchemasFor(db: DB, tableName: string, tableInfo) {
+function createCrrSchemasFor(
+  db: DB,
+  tableName: string,
+  tableInfo: TableInfo,
+  indexList
+) {
   console.log("\n");
   console.log(chalk.green("Creating LWW table for", chalk.blue(tableName)));
+
+  db.prepare(`CREATE TABLE IF NOT EXISTS ${tableName}_crr (
+    ${tableInfo.map(getColumnDefinition).join(",\n")}
+  )`);
+
+  console.log(`\tCreating indices`);
+
+  // TODO: some indices may need to be dropped. warn on that.
 
   console.log(chalk.green("Creating Clock table for", chalk.blue(tableName)));
 
@@ -31,4 +59,12 @@ function createCrrSchemasFor(db: DB, tableName: string, tableInfo) {
   console.log(chalk.green("Creating patch view for", chalk.blue(tableName)));
 
   console.log(chalk.green("Creating triggers for", chalk.blue(tableName)));
+}
+
+function getColumnDefinition(columnInfo: ColumnInfo) {
+  return `"${columnInfo.name}" ${columnInfo.type}${
+    columnInfo.notnull === 1 ? " NOT NULL" : ""
+  }${
+    columnInfo.dflt_value !== null ? ` DEFAULT ${columnInfo.dflt_value}` : ""
+  }${columnInfo.pk === 1 ? " PRIMARY KEY" : ""}`;
 }

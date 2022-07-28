a20fed9be44c0a295658e7fafaca3eaa83a3cd32
Matt <1009003+tantaman@users.noreply.github.com>
2022-07-27 21:50:14 -0400

    create indices and views

--- prototype/migrator/src/migrateTableSchema.ts
+++ prototype/migrator/src/migrateTableSchema.ts
@@ -12,7 +12,7 @@ type ColumnInfo = {
 type TableInfo = ColumnInfo[];
 
 // https://www.sqlite.org/pragma.html#pragma_index_info
-type IndexInfo = {
+type IndexListEntry = {
   seq: number;
   name: string;
   unique: number;
@@ -27,6 +27,7 @@ export default function migrateTableSchemas(
 ) {
   which.forEach((w) =>
     createCrrSchemasFor(
+      src,
       dest,
       w,
       src.pragma(`table_info(${w})`),
@@ -36,35 +37,110 @@ export default function migrateTableSchemas(
 }
 
 function createCrrSchemasFor(
-  db: DB,
+  src: DB,
+  dest: DB,
   tableName: string,
   tableInfo: TableInfo,
-  indexList
+  indexList: IndexListEntry[]
 ) {
   console.log("\n");
   console.log(chalk.green("Creating LWW table for", chalk.blue(tableName)));
 
-  db.prepare(`CREATE TABLE IF NOT EXISTS ${tableName}_crr (
-    ${tableInfo.map(getColumnDefinition).join(",\n")}
-  )`);
+  dest
+    .prepare(
+      `CREATE TABLE IF NOT EXISTS "${tableName}_crr" (
+    ${tableInfo.flatMap(getColumnDefinition).join(",\n")}
+  )`
+    )
+    .run();
 
   console.log(`\tCreating indices`);
+  indexList.forEach((index) => {
+    const indexInfo = src.pragma(`index_info("${index.name}")`);
+    if (index.unique === 1 && index.origin != "pk") {
+      console.log(
+        chalk.yellow(
+          "\tWARNING: unique indices that are not primary keys are dropped and treated as non-unique indices."
+        )
+      );
+      console.log(
+        chalk.yellow(
+          `Converted ${tableName}.${index.name} to a non-unique index`
+        )
+      );
+    }
 
-  // TODO: some indices may need to be dropped. warn on that.
+    console.log(`\tcreating index ${index.name} on ${tableName}_crr`);
+    dest
+      .prepare(
+        `CREATE INDEX IF NOT EXISTS "${
+          index.name
+        }" ON "${tableName}_crr" (${indexInfo
+          .map((i) => '"' + i.name + '"')
+          .join(",")})`
+      )
+      .run();
+  });
 
   console.log(chalk.green("Creating Clock table for", chalk.blue(tableName)));
+  dest
+    .prepare(
+      `CREATE TABLE IF NOT EXISTS "${tableName}_crr_clocks" (
+    "id" integer NOT NULL,
+    "siteId" integer NOT NULL,
+    "version" integer NOT NULL,
+    PRIMARY KEY ("siteId", "id")
+  )`
+    )
+    .run();
 
   console.log(chalk.green("Creating view for", chalk.blue(tableName)));
+  dest
+    .prepare(
+      `CREATE VIEW IF NOT EXISTS "${tableName}" AS SELECT ${tableInfo
+        .map((t) => `"${t.name}"`)
+        .join(",\n")}
+      FROM
+        "${tableName}_crr"
+      WHERE
+        "${tableName}_crr"."crr_cl" % 2 = 1`
+    )
+    .run();
 
   console.log(chalk.green("Creating patch view for", chalk.blue(tableName)));
+  dest.prepare(
+    `CREATE VIEW
+      IF NOT EXISTS "${tableName}_patch" AS SELECT
+        "${tableName}_crr".*,
+        '{"fake": 1}' as crr_clock
+      FROM "${tableName}_crr"`
+  );
 
   console.log(chalk.green("Creating triggers for", chalk.blue(tableName)));
+  // createInsertTrigger();
+  // createUpdateTrigger();
+  // createDeleteTrigger();
+  // createPatchTrigger();
 }
 
-function getColumnDefinition(columnInfo: ColumnInfo) {
-  return `"${columnInfo.name}" ${columnInfo.type}${
-    columnInfo.notnull === 1 ? " NOT NULL" : ""
-  }${
-    columnInfo.dflt_value !== null ? ` DEFAULT ${columnInfo.dflt_value}` : ""
-  }${columnInfo.pk === 1 ? " PRIMARY KEY" : ""}`;
+function getColumnDefinition(columnInfo: ColumnInfo): string[] {
+  if (columnInfo.pk === 1) {
+    return [
+      `"${columnInfo.name}" ${columnInfo.type}${
+        columnInfo.notnull === 1 ? " NOT NULL" : ""
+      }${
+        columnInfo.dflt_value !== null
+          ? ` DEFAULT '${columnInfo.dflt_value}'`
+          : ""
+      } PRIMARY KEY`,
+    ];
+  }
+  return [
+    `"${columnInfo.name}" ${columnInfo.type}${
+      columnInfo.notnull === 1 ? " NOT NULL" : ""
+    }${
+      columnInfo.dflt_value !== null ? ` DEFAULT ${columnInfo.dflt_value}` : ""
+    }`,
+    `"${columnInfo.name}_v" INTEGER DEFAULT 0`,
+  ];
 }

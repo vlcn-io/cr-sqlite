1dd83200f0c8bb873a2e01db89cdffd431609f85
Matt <1009003+tantaman@users.noreply.github.com>
2022-07-29 10:47:49 -0400

    take input, save command history

--- prototype/examples/music-store/src/App.tsx
+++ prototype/examples/music-store/src/App.tsx
@@ -1,9 +1,54 @@
 import { DB, Notifier } from "./createDb";
+import { createSignal, For } from "solid-js";
+
+const prompt = "sql> ";
+const [commands, setCommands] = createSignal<string[]>([]);
 
 export default function App({ db, notifier }: { db: DB; notifier: Notifier }) {
-  return <div>Hello</div>;
+  return <Term />;
 }
 
 function Term() {
-  return <div></div>;
+  return (
+    <div class="term">
+      <Output />
+      <Input />
+    </div>
+  );
+}
+
+function Output() {
+  return (
+    <For each={commands()}>
+      {(cmd, i) => (
+        <div>
+          {prompt}
+          {cmd}
+        </div>
+      )}
+    </For>
+  );
+}
+
+function Input() {
+  const [cmd, setCmd] = createSignal("");
+  function processCommand(e) {
+    e.preventDefault();
+    setCommands((prev) => [...prev, cmd()]);
+    setCmd("");
+    return false;
+  }
+
+  return (
+    <div>
+      {prompt}
+      <form onSubmit={processCommand}>
+        <input
+          type="text"
+          onChange={(e) => setCmd((e.target as any).value)}
+          value={cmd()}
+        ></input>
+      </form>
+    </div>
+  );
 }

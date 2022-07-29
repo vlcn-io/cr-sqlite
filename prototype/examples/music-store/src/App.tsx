713ca620183f26f5393ee04be6cfe80bdcfd2bbc
Matt <1009003+tantaman@users.noreply.github.com>
2022-07-29 11:05:41 -0400

    run commands

--- prototype/examples/music-store/src/App.tsx
+++ prototype/examples/music-store/src/App.tsx
@@ -5,29 +5,62 @@ const prompt = "sql> ";
 const [commands, setCommands] = createSignal<string[]>([]);
 
 export default function App({ db, notifier }: { db: DB; notifier: Notifier }) {
-  return <Term />;
+  return <Term db={db} notifier={notifier} />;
 }
 
-function Term() {
+function Term({ db, notifier }: { db: DB; notifier: Notifier }) {
   return (
     <div class="term">
-      <Output />
+      <Output db={db} notifier={notifier} />
       <Input />
     </div>
   );
 }
 
-function Output() {
+function Output({ db, notifier }: { db: DB; notifier: Notifier }) {
   return (
     <For each={commands()}>
-      {(cmd, i) => (
+      {(cmd, i) => <Cell cmd={cmd} db={db} notifier={notifier} />}
+    </For>
+  );
+}
+
+function Cell({
+  cmd,
+  db,
+  notifier,
+}: {
+  cmd: string;
+  db: DB;
+  notifier: Notifier;
+}) {
+  console.log("execing " + cmd);
+
+  // also do our subscribing......
+  // I guess `result` would need to be a signal updatable by notifier.
+  try {
+    assertAllowed(cmd);
+    const result = db.exec(cmd);
+    return (
+      <div>
         <div>
           {prompt}
           {cmd}
         </div>
-      )}
-    </For>
-  );
+        <div>future result...</div>
+      </div>
+    );
+  } catch (e) {
+    return (
+      <div>
+        <div>
+          {prompt}
+          {cmd}
+        </div>
+        <div>{e.message}</div>
+      </div>
+    );
+  }
 }
 
 function Input() {
@@ -40,15 +73,31 @@ function Input() {
   }
 
   return (
-    <div>
-      {prompt}
+    <div class="input">
+      <span class="prompt">{prompt}</span>
       <form onSubmit={processCommand}>
         <input
           type="text"
           onChange={(e) => setCmd((e.target as any).value)}
           value={cmd()}
+          autofocus
         ></input>
       </form>
     </div>
   );
+}
+
+function assertAllowed(cmd: string) {
+  cmd = cmd.trim().toLowerCase();
+  const allowed =
+    cmd.startsWith("insert") ||
+    cmd.startsWith("update") ||
+    cmd.startsWith("select") ||
+    cmd.startsWith("delete");
+
+  if (!allowed) {
+    throw new Error(
+      "Only select / insert / update / delete queries may be run."
+    );
+  }
 }

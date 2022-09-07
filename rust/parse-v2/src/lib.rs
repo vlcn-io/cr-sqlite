648684a1b9d106df377faeeec4ee5718b02ee38e
Matt <1009003+tantaman@users.noreply.github.com>
2022-09-07 10:33:30 -0400

    use lemon-rs
    Finally ran across a project that fully supports parsing sqlite in rust via a port of lemon

--- rust/parse-v2/src/lib.rs
+++ rust/parse-v2/src/lib.rs
@@ -1,35 +1,23 @@
-peg::parser!{
-  grammar sql_parser() for str {
-    #[cache_left_rec]
-    pub rule statements() -> Vec<String>
-      = l:statements() _ ";" _ r:statement() { l.into_iter().chain(vec![r].into_iter()).collect() }
-      / s:statement() { vec![s] }
-    
-
-    rule statement() -> String
-      = s:$(['a'..='z']+) { s.parse().unwrap() }
-    
-    rule _() = quiet!{[' ' | '\n' | '\t']*}
-  }
-}
-
 #[cfg(test)]
 mod tests {
-  use crate::sql_parser;
+  use fallible_iterator::FallibleIterator;
+  use sqlite3_parser::lexer::sql::Parser;
 
   #[test]
   fn it_works() {
-    let cases = vec![
-      ("foo", vec!["foo"]),
-      ("foo;", vec!["foo"]),
-      ("foo;bar", vec!["foo", "bar"]),
-      ("foo; bar", vec!["foo", "bar"])
-    ];
-
-
-    for case in cases {
-      let parsed = sql_parser::statements(case[0]);
-      assert_eq!(parsed, Ok(case[1]));
+    let arg = "CREATE TABLE foo (a, b, c); CREATE TABLE bar (d, e, f);";
+    let mut parser = Parser::new(arg.as_bytes());
+    loop {
+      match parser.next() {
+        Ok(None) => break,
+        Err(err) => {
+          eprintln!("Err: {} in {}", err, arg);
+          break;
+        }
+        Ok(Some(cmd)) => {
+          println!("{}", cmd);
+        }
+      }
     }
   }
 }

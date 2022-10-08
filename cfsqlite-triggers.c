3f22329dd37cc91f7180ff25150bd9f604d44a45
Matt <1009003+tantaman@users.noreply.github.com>
2022-10-08 13:41:18 -0400

    declarations for trigger methods to implement

--- cfsqlite-triggers.c
+++ cfsqlite-triggers.c
@@ -4,13 +4,16 @@
 #include <stdint.h>
 #include <string.h>
 
-char *cfsql_conflictSetsStr(cfsql_ColumnInfo *cols, int len) {
+char *cfsql_conflictSetsStr(cfsql_ColumnInfo *cols, int len)
+{
   return 0;
 }
 
-char *cfsql_localInsertConflictResolutionStr(cfsql_TableInfo *tableInfo) {
-  if (tableInfo->pksLen == 0) {
-    // dup given the caller would try to deallocate it and we 
+char *cfsql_localInsertConflictResolutionStr(cfsql_TableInfo *tableInfo)
+{
+  if (tableInfo->pksLen == 0)
+  {
+    // dup given the caller would try to deallocate it and we
     // cannot deallocate a literal
     return strdup("");
   }
@@ -18,14 +21,13 @@ char *cfsql_localInsertConflictResolutionStr(cfsql_TableInfo *tableInfo) {
   char *pkList = cfsql_asIdentifierList(tableInfo->pks, tableInfo->pksLen, 0);
   char *conflictSets = cfsql_conflictSetsStr(tableInfo->nonPks, tableInfo->nonPksLen);
 
-  char * ret = sqlite3_mprintf(
-    "ON CONFLICT (%s) DO UPDATE SET\
+  char *ret = sqlite3_mprintf(
+      "ON CONFLICT (%s) DO UPDATE SET\
       %s\
     \"__cfsql_cl\" = CASE WHEN \"__cfsql_cl\" % 2 = 0 THEN \"__cfsql_cl\" + 1 ELSE \"__cfsql_cl\" END,\
     \"__cfsql_src\" = 0",
-    pkList,
-    conflictSets
-  );
+      pkList,
+      conflictSets);
 
   sqlite3_free(pkList);
   sqlite3_free(conflictSets);
@@ -33,20 +35,16 @@ char *cfsql_localInsertConflictResolutionStr(cfsql_TableInfo *tableInfo) {
   return ret;
 }
 
-char *cfsql_updateClocksStr(cfsql_TableInfo *tableInfo) {
+char *cfsql_updateClocksStr(cfsql_TableInfo *tableInfo)
+{
   return 0;
 }
 
-int cfsql_createCrrViewTriggers(
+int cfsql_createInsertTrigger(
     sqlite3 *db,
     cfsql_TableInfo *tableInfo,
     char **err)
 {
-
-  // cfsql_createInsertTrigger();
-  // cfsql_createUpdateTrigger();
-  // cfsql_createDeleteTrigger();
-
   char *zSql;
   char *baseColumnsList = 0;
   char *baseColumnsNewList = 0;
@@ -59,7 +57,7 @@ int cfsql_createCrrViewTriggers(
   updateClocks = cfsql_updateClocksStr(tableInfo);
 
   zSql = sqlite3_mprintf(
-    "CREATE TRIGGER \"%s__cfsql_itrig\"\
+      "CREATE TRIGGER \"%s__cfsql_itrig\"\
       INSTEAD OF INSERT ON \"%s\"\
     BEGIN\
       INSERT INTO \"%s__cfsql_crr\" (\
@@ -69,14 +67,13 @@ int cfsql_createCrrViewTriggers(
       ) %s;\
       %s\
     END;",
-    tableInfo->tblName,
-    tableInfo->tblName,
-    tableInfo->tblName,
-    baseColumnsList,
-    baseColumnsNewList,
-    conflictResolution,
-    updateClocks
-  );
+      tableInfo->tblName,
+      tableInfo->tblName,
+      tableInfo->tblName,
+      baseColumnsList,
+      baseColumnsNewList,
+      conflictResolution,
+      updateClocks);
   // exec
 
   sqlite3_free(zSql);
@@ -85,7 +82,44 @@ int cfsql_createCrrViewTriggers(
   sqlite3_free(conflictResolution);
   sqlite3_free(updateClocks);
 
-  return 0;
+  return SQLITE_OK;
+}
+
+int cfsql_createUpdateTrigger(sqlite3 *db,
+                              cfsql_TableInfo *tableInfo,
+                              char **err)
+{
+  return SQLITE_OK;
+}
+
+int cfsql_createDeleteTrigger(
+    sqlite3 *db,
+    cfsql_TableInfo *tableInfo,
+    char **err)
+{
+  // char *zSql = sqlite3_mprintf(
+  //   "CREATE TRIGGER"
+  // );
+  return SQLITE_OK;
+}
+
+int cfsql_createCrrViewTriggers(
+    sqlite3 *db,
+    cfsql_TableInfo *tableInfo,
+    char **err)
+{
+
+  int rc = cfsql_createInsertTrigger(db, tableInfo, err);
+  if (rc == SQLITE_OK)
+  {
+    rc = cfsql_createUpdateTrigger(db, tableInfo, err);
+  }
+  if (rc == SQLITE_OK)
+  {
+    rc = cfsql_createDeleteTrigger(db, tableInfo, err);
+  }
+
+  return rc;
 }
 
 int cfsql_createPatchTrigger(

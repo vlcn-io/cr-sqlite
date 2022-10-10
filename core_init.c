9a43bae59e90cb69f35e972505cc3a628dde7ad3
Matt <1009003+tantaman@users.noreply.github.com>
2022-10-10 10:10:48 -0400

    add pre-init method
    This'll be used to set up mutexes to guard global state that is shared between
    connections.

--- core_init.c
+++ core_init.c
@@ -1,6 +1,6 @@
 /*
   This file is appended to the end of a sqlite3.c amalgammation
-  file to include sqlite3_path functions/tables statically in
+  file to include cfsqlite functions statically in
   a build. This is used for the demo CLI and WASM implementations.
 */
 #include "cfsqlite-ext.h"
@@ -8,11 +8,14 @@
 
 int core_init(const char *dummy) {
   int rc = SQLITE_OK;
-  rc = sqlite3_auto_extension((void *)sqlite3_uuid_init);
 
-  if (rc != SQLITE_OK) {
-    return rc;
+  rc = sqlite3_cfsqlite_preinit();
+  if (rc == SQLITE_OK) {
+    rc = sqlite3_auto_extension((void *)sqlite3_uuid_init);
   }
-
-  return sqlite3_auto_extension((void *)sqlite3_cfsqlite_init);
+  if (rc == SQLITE_OK) {
+    rc = sqlite3_auto_extension((void *)sqlite3_cfsqlite_init);
+  }
+  
+  return rc;
 }

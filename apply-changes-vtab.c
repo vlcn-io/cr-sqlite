b5be337e9c4a7dc015d99e9fc85b9f76d49d5a52
Matt <1009003+tantaman@users.noreply.github.com>
2022-10-24 08:45:24 -0400

    rename template vtab methods to apply-changes-vtab

--- apply-changes-vtab.c
+++ apply-changes-vtab.c
@@ -1,60 +1,59 @@
-#if !defined(SQLITEINT_H)
-#include "sqlite3ext.h"
-#endif
-SQLITE_EXTENSION_INIT1
+#include "apply-changes-vtab.h"
 #include <string.h>
 #include <assert.h>
+#include "consts.h"
+#include "util.h"
 
-/* templatevtab_vtab is a subclass of sqlite3_vtab which is
+/* crsql_applyChanges_vtab is a subclass of sqlite3_vtab which is
 ** underlying representation of the virtual table
 */
-typedef struct templatevtab_vtab templatevtab_vtab;
-struct templatevtab_vtab {
+typedef struct crsql_applyChanges_vtab crsql_applyChanges_vtab;
+struct crsql_applyChanges_vtab {
   sqlite3_vtab base;  /* Base class - must be first */
   /* Add new fields here, as necessary */
 };
 
-/* templatevtab_cursor is a subclass of sqlite3_vtab_cursor which will
+/* crsql_applyChanges_cursor is a subclass of sqlite3_vtab_cursor which will
 ** serve as the underlying representation of a cursor that scans
 ** over rows of the result
 */
-typedef struct templatevtab_cursor templatevtab_cursor;
-struct templatevtab_cursor {
+typedef struct crsql_applyChanges_cursor crsql_applyChanges_cursor;
+struct crsql_applyChanges_cursor {
   sqlite3_vtab_cursor base;  /* Base class - must be first */
-  /* Insert new fields here.  For this templatevtab we only keep track
+  /* Insert new fields here.  For this crsql_applyChanges we only keep track
   ** of the rowid */
   sqlite3_int64 iRowid;      /* The rowid */
 };
 
 /*
-** The templatevtabConnect() method is invoked to create a new
+** The crsql_applyChangesConnect() method is invoked to create a new
 ** template virtual table.
 **
-** Think of this routine as the constructor for templatevtab_vtab objects.
+** Think of this routine as the constructor for crsql_applyChanges_vtab objects.
 **
 ** All this routine needs to do is:
 **
-**    (1) Allocate the templatevtab_vtab object and initialize all fields.
+**    (1) Allocate the crsql_applyChanges_vtab object and initialize all fields.
 **
 **    (2) Tell SQLite (via the sqlite3_declare_vtab() interface) what the
 **        result set of queries against the virtual table will look like.
 */
-static int templatevtabConnect(
+static int applyChangesConnect(
   sqlite3 *db,
   void *pAux,
   int argc, const char *const*argv,
   sqlite3_vtab **ppVtab,
   char **pzErr
 ){
-  templatevtab_vtab *pNew;
+  crsql_applyChanges_vtab *pNew;
   int rc;
 
   rc = sqlite3_declare_vtab(db,
            "CREATE TABLE x(a,b)"
        );
   /* For convenience, define symbolic names for the index to each column. */
-#define TEMPLATEVTAB_A  0
-#define TEMPLATEVTAB_B  1
+#define crsql_applyChanges_A  0
+#define crsql_applyChanges_B  1
   if( rc==SQLITE_OK ){
     pNew = sqlite3_malloc( sizeof(*pNew) );
     *ppVtab = (sqlite3_vtab*)pNew;
@@ -65,19 +64,19 @@ static int templatevtabConnect(
 }
 
 /*
-** This method is the destructor for templatevtab_vtab objects.
+** This method is the destructor for crsql_applyChanges_vtab objects.
 */
-static int templatevtabDisconnect(sqlite3_vtab *pVtab){
-  templatevtab_vtab *p = (templatevtab_vtab*)pVtab;
+static int applyChangesDisconnect(sqlite3_vtab *pVtab){
+  crsql_applyChanges_vtab *p = (crsql_applyChanges_vtab*)pVtab;
   sqlite3_free(p);
   return SQLITE_OK;
 }
 
 /*
-** Constructor for a new templatevtab_cursor object.
+** Constructor for a new crsql_applyChanges_cursor object.
 */
-static int templatevtabOpen(sqlite3_vtab *p, sqlite3_vtab_cursor **ppCursor){
-  templatevtab_cursor *pCur;
+static int applyChangesOpen(sqlite3_vtab *p, sqlite3_vtab_cursor **ppCursor){
+  crsql_applyChanges_cursor *pCur;
   pCur = sqlite3_malloc( sizeof(*pCur) );
   if( pCur==0 ) return SQLITE_NOMEM;
   memset(pCur, 0, sizeof(*pCur));
@@ -86,40 +85,40 @@ static int templatevtabOpen(sqlite3_vtab *p, sqlite3_vtab_cursor **ppCursor){
 }
 
 /*
-** Destructor for a templatevtab_cursor.
+** Destructor for a crsql_applyChanges_cursor.
 */
-static int templatevtabClose(sqlite3_vtab_cursor *cur){
-  templatevtab_cursor *pCur = (templatevtab_cursor*)cur;
+static int applyChangesClose(sqlite3_vtab_cursor *cur){
+  crsql_applyChanges_cursor *pCur = (crsql_applyChanges_cursor*)cur;
   sqlite3_free(pCur);
   return SQLITE_OK;
 }
 
 
 /*
-** Advance a templatevtab_cursor to its next row of output.
+** Advance a crsql_applyChanges_cursor to its next row of output.
 */
-static int templatevtabNext(sqlite3_vtab_cursor *cur){
-  templatevtab_cursor *pCur = (templatevtab_cursor*)cur;
+static int applyChangesNext(sqlite3_vtab_cursor *cur){
+  crsql_applyChanges_cursor *pCur = (crsql_applyChanges_cursor*)cur;
   pCur->iRowid++;
   return SQLITE_OK;
 }
 
 /*
-** Return values of columns for the row at which the templatevtab_cursor
+** Return values of columns for the row at which the crsql_applyChanges_cursor
 ** is currently pointing.
 */
-static int templatevtabColumn(
+static int applyChangesColumn(
   sqlite3_vtab_cursor *cur,   /* The cursor */
   sqlite3_context *ctx,       /* First argument to sqlite3_result_...() */
   int i                       /* Which column to return */
 ){
-  templatevtab_cursor *pCur = (templatevtab_cursor*)cur;
+  crsql_applyChanges_cursor *pCur = (crsql_applyChanges_cursor*)cur;
   switch( i ){
-    case TEMPLATEVTAB_A:
+    case crsql_applyChanges_A:
       sqlite3_result_int(ctx, 1000 + pCur->iRowid);
       break;
     default:
-      assert( i==TEMPLATEVTAB_B );
+      assert( i==crsql_applyChanges_B );
       sqlite3_result_int(ctx, 2000 + pCur->iRowid);
       break;
   }
@@ -130,8 +129,8 @@ static int templatevtabColumn(
 ** Return the rowid for the current row.  In this implementation, the
 ** rowid is the same as the output value.
 */
-static int templatevtabRowid(sqlite3_vtab_cursor *cur, sqlite_int64 *pRowid){
-  templatevtab_cursor *pCur = (templatevtab_cursor*)cur;
+static int applyChangesRowid(sqlite3_vtab_cursor *cur, sqlite_int64 *pRowid){
+  crsql_applyChanges_cursor *pCur = (crsql_applyChanges_cursor*)cur;
   *pRowid = pCur->iRowid;
   return SQLITE_OK;
 }
@@ -140,23 +139,23 @@ static int templatevtabRowid(sqlite3_vtab_cursor *cur, sqlite_int64 *pRowid){
 ** Return TRUE if the cursor has been moved off of the last
 ** row of output.
 */
-static int templatevtabEof(sqlite3_vtab_cursor *cur){
-  templatevtab_cursor *pCur = (templatevtab_cursor*)cur;
+static int applyChangesEof(sqlite3_vtab_cursor *cur){
+  crsql_applyChanges_cursor *pCur = (crsql_applyChanges_cursor*)cur;
   return pCur->iRowid>=10;
 }
 
 /*
-** This method is called to "rewind" the templatevtab_cursor object back
+** This method is called to "rewind" the crsql_applyChanges_cursor object back
 ** to the first row of output.  This method is always called at least
-** once prior to any call to templatevtabColumn() or templatevtabRowid() or 
-** templatevtabEof().
+** once prior to any call to crsql_applyChangesColumn() or crsql_applyChangesRowid() or 
+** crsql_applyChangesEof().
 */
-static int templatevtabFilter(
+static int applyChangesFilter(
   sqlite3_vtab_cursor *pVtabCursor, 
   int idxNum, const char *idxStr,
   int argc, sqlite3_value **argv
 ){
-  templatevtab_cursor *pCur = (templatevtab_cursor *)pVtabCursor;
+  crsql_applyChanges_cursor *pCur = (crsql_applyChanges_cursor *)pVtabCursor;
   pCur->iRowid = 1;
   return SQLITE_OK;
 }
@@ -167,7 +166,7 @@ static int templatevtabFilter(
 ** a query plan for each invocation and compute an estimated cost for that
 ** plan.
 */
-static int templatevtabBestIndex(
+static int applyChangesBestIndex(
   sqlite3_vtab *tab,
   sqlite3_index_info *pIdxInfo
 ){
@@ -180,20 +179,20 @@ static int templatevtabBestIndex(
 ** This following structure defines all the methods for the 
 ** virtual table.
 */
-static sqlite3_module templatevtabModule = {
+static sqlite3_module crsql_applyChangesModule = {
   /* iVersion    */ 0,
   /* xCreate     */ 0,
-  /* xConnect    */ templatevtabConnect,
-  /* xBestIndex  */ templatevtabBestIndex, // since insert will take where
-  /* xDisconnect */ templatevtabDisconnect,
+  /* xConnect    */ applyChangesConnect,
+  /* xBestIndex  */ applyChangesBestIndex, // since insert will take where
+  /* xDisconnect */ applyChangesDisconnect,
   /* xDestroy    */ 0,
-  /* xOpen       */ templatevtabOpen,
-  /* xClose      */ templatevtabClose,
-  /* xFilter     */ templatevtabFilter,
-  /* xNext       */ templatevtabNext,
-  /* xEof        */ templatevtabEof,
-  /* xColumn     */ templatevtabColumn,
-  /* xRowid      */ templatevtabRowid,
+  /* xOpen       */ applyChangesOpen,
+  /* xClose      */ applyChangesClose,
+  /* xFilter     */ applyChangesFilter,
+  /* xNext       */ applyChangesNext,
+  /* xEof        */ applyChangesEof,
+  /* xColumn     */ applyChangesColumn,
+  /* xRowid      */ applyChangesRowid,
   /* xUpdate     */ 0,
   /* xBegin      */ 0,
   /* xSync       */ 0,
---
+++ apply-changes-vtab.h
@@ -0,0 +1,5 @@
+#if !defined(SQLITEINT_H)
+#include "sqlite3ext.h"
+#endif
+SQLITE_EXTENSION_INIT3
+

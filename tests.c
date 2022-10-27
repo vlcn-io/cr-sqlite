df8a17e52478df81ca4a49d7308b42b167d667a9
Matt <1009003+tantaman@users.noreply.github.com>
2022-10-27 11:50:10 -0400

    allowing runnig suites independently

--- tests.c
+++ tests.c
@@ -1,3 +1,8 @@
+#include <stdio.h>
+#include <string.h>
+
+#define SUITE(N) if (strcmp(suite, "all") == 0 || strcmp(suite, N) == 0)
+
 void crsqlUtilTestSuite();
 void crsqlTableInfoTestSuite();
 void crsqlTestSuite();
@@ -9,12 +14,17 @@ void crsqlChangesVtabCommonTestSuite();
 
 int main(int argc, char *argv[])
 {
-  crsqlUtilTestSuite();
-  crsqlTableInfoTestSuite();
-  crsqlTestSuite();
-  crsqlTriggersTestSuite();
-  crsqlChangesVtabTestSuite();
-  crsqlChangesVtabReadTestSuite();
-  crsqlChangesVtabWriteTestSuite();
-  crsqlChangesVtabCommonTestSuite();
+  char * suite = "all";
+  if (argc == 2) {
+    suite = argv[1];
+  }
+
+  SUITE("util") crsqlUtilTestSuite();
+  SUITE("tblinfo") crsqlTableInfoTestSuite();
+  SUITE("crsql") crsqlTestSuite();
+  SUITE("triggers") crsqlTriggersTestSuite();
+  SUITE("vtab") crsqlChangesVtabTestSuite();
+  SUITE("vtabread") crsqlChangesVtabReadTestSuite();
+  SUITE("vtabwrite") crsqlChangesVtabWriteTestSuite();
+  SUITE("vtabcommon") crsqlChangesVtabCommonTestSuite();
 }

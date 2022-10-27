5660568945969967e77c40a0e32a942e71857824
Matt <1009003+tantaman@users.noreply.github.com>
2022-10-27 09:33:58 -0400

    move vtab stucts to header

--- changes-vtab.c
+++ changes-vtab.c
@@ -42,80 +42,6 @@
 #include "crsqlite.h"
 
 /**
- * Data maintained by the virtual table across
- * queries.
- *
- * Per-query data is kept on crsql_Changes_cursor
- *
- * All table infos are fetched on vtab initialization.
- * This creates the constraint that if the schema of a crr
- * is modified after the virtual table definition is loaded
- * then it will not be or not be correctly processed
- * by the virtual table.
- *
- * Given that, if a schema modification is made
- * to a crr table then the changes vtab needs to be
- * reloaded.
- *
- * The simpleset way to accomplish this is to close
- * and re-open the connection responsible for syncing.
- *
- * In practice this should generally not be a problem
- * as application startup would establish, migrated, etc. the schemas
- * after which a sync process would connect.
- */
-typedef struct crsql_Changes_vtab crsql_Changes_vtab;
-struct crsql_Changes_vtab
-{
-  sqlite3_vtab base;
-  sqlite3 *db;
-
-  crsql_TableInfo **tableInfos;
-  int tableInfosLen;
-
-  int64_t maxSeenPatchVersion;
-};
-
-/**
- * Cursor used to return patches.
- * This is instantiated per-query and updated
- * on each row being returned.
- *
- * Contains a reference to the vtab structure in order
- * get a handle on the db which to fetch from
- * the underlying crr tables.
- *
- * Most columns are passed-through from
- * `pChangesStmt` and `pRowStmt` which are stepped
- * in each call to `changesNext`.
- *
- * `colVersion` is copied given it is unclear
- * what the behavior is of calling `sqlite3_column_x` on
- * the same column multiple times with, potentially,
- * different types.
- *
- * `colVersions` is used in the implementation as
- * a text column in order to fetch the correct columns
- * from the physical row.
- *
- * Everything allocated here must be constructed in
- * changesOpen and released in changesCrsrFinalize
- */
-typedef struct crsql_Changes_cursor crsql_Changes_cursor;
-struct crsql_Changes_cursor
-{
-  sqlite3_vtab_cursor base;
-
-  crsql_Changes_vtab *pTab;
-
-  sqlite3_stmt *pChangesStmt;
-  sqlite3_stmt *pRowStmt;
-
-  const char *colVrsns;
-  sqlite3_int64 version;
-};
-
-/**
  * Created when the virtual table is initialized.
  * This happens when the vtab is first used in a given connection.
  * The method allocated the crsql_Changes_vtab for use for the duration
@@ -1238,7 +1164,7 @@ int crsql_mergeInsert(
   return SQLITE_OK;
 }
 
-int changesApply(
+static int changesApply(
     sqlite3_vtab *pVTab,
     int argc,
     sqlite3_value **argv,
--- changes-vtab.h
+++ changes-vtab.h
@@ -4,8 +4,83 @@
 SQLITE_EXTENSION_INIT3
 
 #include "tableinfo.h"
+#include <stdint.h>
 
 sqlite3_module crsql_changesModule;
+
+/**
+ * Data maintained by the virtual table across
+ * queries.
+ *
+ * Per-query data is kept on crsql_Changes_cursor
+ *
+ * All table infos are fetched on vtab initialization.
+ * This creates the constraint that if the schema of a crr
+ * is modified after the virtual table definition is loaded
+ * then it will not be or not be correctly processed
+ * by the virtual table.
+ *
+ * Given that, if a schema modification is made
+ * to a crr table then the changes vtab needs to be
+ * reloaded.
+ *
+ * The simpleset way to accomplish this is to close
+ * and re-open the connection responsible for syncing.
+ *
+ * In practice this should generally not be a problem
+ * as application startup would establish, migrated, etc. the schemas
+ * after which a sync process would connect.
+ */
+typedef struct crsql_Changes_vtab crsql_Changes_vtab;
+struct crsql_Changes_vtab
+{
+  sqlite3_vtab base;
+  sqlite3 *db;
+
+  crsql_TableInfo **tableInfos;
+  int tableInfosLen;
+
+  int64_t maxSeenPatchVersion;
+};
+
+/**
+ * Cursor used to return patches.
+ * This is instantiated per-query and updated
+ * on each row being returned.
+ *
+ * Contains a reference to the vtab structure in order
+ * get a handle on the db which to fetch from
+ * the underlying crr tables.
+ *
+ * Most columns are passed-through from
+ * `pChangesStmt` and `pRowStmt` which are stepped
+ * in each call to `changesNext`.
+ *
+ * `colVersion` is copied given it is unclear
+ * what the behavior is of calling `sqlite3_column_x` on
+ * the same column multiple times with, potentially,
+ * different types.
+ *
+ * `colVersions` is used in the implementation as
+ * a text column in order to fetch the correct columns
+ * from the physical row.
+ *
+ * Everything allocated here must be constructed in
+ * changesOpen and released in changesCrsrFinalize
+ */
+typedef struct crsql_Changes_cursor crsql_Changes_cursor;
+struct crsql_Changes_cursor
+{
+  sqlite3_vtab_cursor base;
+
+  crsql_Changes_vtab *pTab;
+
+  sqlite3_stmt *pChangesStmt;
+  sqlite3_stmt *pRowStmt;
+
+  const char *colVrsns;
+  sqlite3_int64 version;
+};
 
 char *crsql_changesQueryForTable(crsql_TableInfo *tableInfo);
 char *crsql_changesUnionQuery(

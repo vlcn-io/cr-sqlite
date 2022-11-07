a5d2ef532fad90cbaad97403a569d9d61cf31c47
Matt <1009003+tantaman@users.noreply.github.com>
2022-11-07 09:35:33 -0500

    fixup vite config to enable bigint literals

--- pkg/wasm-esm/examples/src/main-thread.ts
+++ pkg/wasm-esm/examples/src/main-thread.ts
@@ -1,26 +1,30 @@
 import sqliteWasm from "@vlcn.io/crsqlite-wasm";
 import { UuidTool } from "uuid-tool";
 
-const sqlite = await sqliteWasm();
+async function run() {
+  const sqlite = await sqliteWasm();
 
-const db = sqlite.open(":memory:");
+  const db = sqlite.open(":memory:");
 
-// @ts-ignore
-window.db = db;
-let rows: any[] = [];
+  // @ts-ignore
+  window.db = db;
+  let rows: any[] = [];
 
-db.exec("CREATE TABLE foo (a primary key, b);");
-db.exec("SELECT crsql_as_crr('foo');");
-db.exec("INSERT INTO foo VALUES (1, 2);");
-rows = db.execA("select crsql_dbversion();");
-console.log("DB Version: ", rows[0][0]);
-rows = db.execA("select crsql_siteid();");
-console.log("Site ID: ", UuidTool.toString(rows[0][0]));
+  db.exec("CREATE TABLE foo (a primary key, b);");
+  db.exec("SELECT crsql_as_crr('foo');");
+  db.exec("INSERT INTO foo VALUES (1, 2);");
+  rows = db.execA("select crsql_dbversion();");
+  console.log("DB Version: ", rows[0][0]);
+  rows = db.execA("select crsql_siteid();");
+  console.log("Site ID: ", UuidTool.toString(rows[0][0]));
 
-rows = db.execA("select * from crsql_changes();");
-console.log("Changes: ", rows);
+  rows = db.execA("select * from crsql_changes();");
+  console.log("Changes: ", rows);
 
-rows = db.execA("SELECT * FROM foo");
-console.log(rows[0]);
+  rows = db.execA("SELECT * FROM foo");
+  console.log(rows[0]);
 
-db.close();
+  db.close();
+}
+
+run();
--- pkg/wasm-esm/examples/vite.config.js
+++ pkg/wasm-esm/examples/vite.config.js
@@ -4,13 +4,20 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   build: {
+    target: "es2020",
     rollupOptions: {
       input: {
         index: resolve(__dirname, "index.html"),
         mainThread: resolve(__dirname, "main-thread.html"),
         onWorker: resolve(__dirname, "on-worker.html"),
-        comlink: resolve(__dirname, "comlink.html")
+        comlink: resolve(__dirname, "comlink.html"),
       },
+    },
+  },
+
+  optimizeDeps: {
+    esbuildOptions: {
+      target: "es2020",
     },
   },
   server: {

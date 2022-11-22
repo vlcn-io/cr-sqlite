80c07203856fa5b2c1f5fe6cc8288c0f678fc52e
Matt <1009003+tantaman@users.noreply.github.com>
2022-11-22 15:22:42 -0500

    continue ripping out official sqlite wasm

--- js/wasm-esm/crsqlite/README.md
+++ js/wasm-esm/crsqlite/README.md
@@ -10,6 +10,7 @@ The official WASM build of SQLite is quite janky --
 - Bad file locking -- can't use the db from two different tabs or workers
 - Requires COOP headers
 - Can't be used in a shared worker
+- Its build is currently broken on Ubuntu
 
 Given that, `cr-sqlite` is using the `wa-sqlite` WASM build which:
 
@@ -17,3 +18,9 @@ Given that, `cr-sqlite` is using the `wa-sqlite` WASM build which:
 - Can be used in shared workers
 - Can be used from many concurrent tabs
 - Can be used with or without COOP/COEP headers
+
+**When the official SQLite WASM build is stable we'll return to that.**
+
+Not that `wa-crsqlite` is the only component of `crsqlite` that is AGPL licensed. This is the case due to `wa-sqlite` project being AGPL.
+
+Whether or not works that call `wa-sqlite` need to be `AGPL` is a bit unknown given it is two separate processes (wa-sqlite and derived app) interacting.
--- js/wasm-esm/examples/src/support/worker.ts
+++ js/wasm-esm/examples/src/support/worker.ts
@@ -1,21 +1,18 @@
-import sqliteWasm from "@vlcn.io/crsqlite-wasm";
+import sqliteWasm from "@vlcn.io/wa-crsqlite;
 
-import wasmUrl from "@vlcn.io/crsqlite-wasm/dist/sqlite3.wasm?url";
-import proxyUrl from "@vlcn.io/crsqlite-wasm/dist/sqlite3-opfs-async-proxy.js?url";
+// @ts-ignore
+import wasmUrl from "@vlcn.io/wa-crsqlite/wa-sqlite-async.wasm?url";
 
-sqliteWasm({
-  locateWasm: () => wasmUrl,
-  locateProxy: () => proxyUrl,
-}).then((sqlite3) => {
-  const db = sqlite3.open("example-db", "c");
+sqliteWasm((file) => wasmUrl).then(async (sqlite3) => {
+  const db = await sqlite3.open("example-db", "c");
 
-  db.execMany([
+  await db.execMany([
     "CREATE TABLE IF NOT EXISTS baz (a, b);",
     "INSERT INTO baz VALUES (1, 2);",
   ]);
 
-  const rows = db.execA("SELECT * FROM baz");
+  const rows = await db.execA("SELECT * FROM baz");
   console.log(rows);
 
-  db.close();
+  await db.close();
 });

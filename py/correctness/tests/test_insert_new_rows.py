fae8cd1a2513b3e498d603fc2f53711b9676650a
Matt <1009003+tantaman@users.noreply.github.com>
2022-11-23 10:36:54 -0500

    update py tests to account for cid migration

--- py/correctness/tests/test_insert_new_rows.py
+++ py/correctness/tests/test_insert_new_rows.py
@@ -9,9 +9,9 @@ def test_c1_c2_c3_c4_c6_c7_crr_values():
   c.execute("insert into foo values(1, 2)")
   c.commit()
 
-  row = c.execute("select id, __crsql_col_num, __crsql_version, __crsql_site_id from foo__crsql_clock").fetchone()
+  row = c.execute("select id, __crsql_col_name, __crsql_version, __crsql_site_id from foo__crsql_clock").fetchone()
   assert row[0] == 1
-  assert row[1] == 1
+  assert row[1] == 'a'
   assert row[2] == init_version + 1
   assert row[3] == None
   new_version = c.execute("SELECT crsql_dbversion()").fetchone()[0]

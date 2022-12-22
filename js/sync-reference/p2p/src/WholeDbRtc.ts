a366166dd67fbd33a935e63353460e4212c58c2c
Matt <1009003+tantaman@users.noreply.github.com>
2022-12-22 13:14:45 -0500

    collapse rtc and wdb into p2p

--- js/sync-reference/p2p/src/WholeDbRtc.ts
+++ js/sync-reference/p2p/src/WholeDbRtc.ts
@@ -4,7 +4,7 @@ import WDB, {
   SiteIDLocal,
   SiteIDWire,
   WholeDbReplicator,
-} from "@vlcn.io/replicator-wholedb";
+} from "./index.js";
 import { DB, DBAsync } from "@vlcn.io/xplat-api";
 import Peer, { DataConnection } from "peerjs";
 // @ts-ignore

50d64f20ca82be7977f1630d386fb213ef2e6208
Matt <1009003+tantaman@users.noreply.github.com>
2023-01-27 12:07:59 -0500

    rust bundle for single entry to and target for  all crsqlite rust extensions

---
+++ rs/bundle/src/lib.rs
@@ -0,0 +1,83 @@
+#![no_std]
+#![feature(core_intrinsics)]
+#![feature(alloc_error_handler)]
+#![feature(lang_items)]
+
+extern crate alloc;
+
+use core::alloc::Layout;
+use core::panic::PanicInfo;
+use sqlite_nostd::SQLite3Allocator;
+
+#[global_allocator]
+static ALLOCATOR: SQLite3Allocator = SQLite3Allocator {};
+
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    core::intrinsics::abort()
+}
+
+#[alloc_error_handler]
+fn oom(_: Layout) -> ! {
+    core::intrinsics::abort()
+}
+
+#[cfg(not(target_family = "wasm"))]
+#[lang = "eh_personality"]
+extern "C" fn eh_personality() {}
+
+#[cfg(target_family = "wasm")]
+#[no_mangle]
+pub extern "C" fn __rust_alloc(size: usize, align: usize) -> *mut u8 {
+    unsafe { ALLOCATOR.alloc(Layout::from_size_align_unchecked(size, align)) }
+}
+
+#[cfg(target_family = "wasm")]
+#[no_mangle]
+pub extern "C" fn __rust_dealloc(ptr: *mut u8, size: usize, align: usize) {
+    unsafe { ALLOCATOR.dealloc(ptr, Layout::from_size_align_unchecked(size, align)) }
+}
+
+#[cfg(target_family = "wasm")]
+#[no_mangle]
+pub extern "C" fn __rust_realloc(
+    ptr: *mut u8,
+    old_size: usize,
+    size: usize,
+    align: usize,
+) -> *mut u8 {
+    unsafe {
+        ALLOCATOR.realloc(
+            ptr,
+            Layout::from_size_align_unchecked(old_size, align),
+            size,
+        )
+    }
+}
+
+#[cfg(target_family = "wasm")]
+#[no_mangle]
+pub extern "C" fn __rust_alloc_zeroed(size: usize, align: usize) -> *mut u8 {
+    unsafe { ALLOCATOR.alloc_zeroed(Layout::from_size_align_unchecked(size, align)) }
+}
+
+#[cfg(target_family = "wasm")]
+#[no_mangle]
+pub fn __rust_alloc_error_handler(_: Layout) -> ! {
+    core::intrinsics::abort()
+}
+
+#[no_mangle]
+pub extern "C" fn sqlite3_crsqlrustbundle_init(
+    db: *mut sqlite::sqlite3,
+    _err_msg: *mut *mut c_char,
+    api: *mut sqlite::api_routines,
+) -> u32 {
+    sqlite::EXTENSION_INIT2(api);
+
+    // load up all our rust extensions that contribute to the project
+    // - automigrate
+    // - fractional indexing
+    // - rga
+    // - eventually crsql core post port
+}

9517aa17c4943db47eea8cb30b49a2be8099a619
Matt <1009003+tantaman@users.noreply.github.com>
2023-02-13 15:12:44 -0500

    disaggregate omit_load_extension feature

--- core/rs/integration-check/build.rs
+++ core/rs/integration-check/build.rs
@@ -3,8 +3,6 @@ use std::env;
 use std::process::Command;
 
 fn main() {
-    let out_dir = env::var("OUT_DIR").unwrap();
-
     Command::new("make")
         .current_dir("../../")
         .arg("loadable")
@@ -13,11 +11,7 @@ fn main() {
 
     cc::Build::new()
         .file("../../src/sqlite/sqlite3.c")
-        //.include("sqlite3/sqlite3ext.h")
         .include("../../src/sqlite/sqlite3.h")
         .flag("-DSQLITE_CORE")
         .compile("sqlite3");
-
-    println!("cargo:rustc-link-search=native={}", out_dir);
-    println!("cargo:rustc-link-lib=static=sqlite3");
 }
--- core/rs/integration-check/tests/pk_only_tables.rs
+++ core/rs/integration-check/tests/pk_only_tables.rs
@@ -41,22 +41,22 @@ fn insert_pkonly_row() {
 }
 
 fn insert_pkonly_row_impl() -> Result<(), sqlite::ResultCode> {
-    let db_a = opendb()?;
-    let db_b = opendb()?;
+    // let db_a = opendb()?;
+    // let db_b = opendb()?;
 
-    fn setup_schema(db: &ManagedConnection) -> Result<sqlite::ResultCode, sqlite::ResultCode> {
-        db.exec_safe("CREATE TABLE foo (id INTEGER PRIMARY KEY);")?;
-        db.exec_safe("SELECT crsql_as_crr('foo');")
-    }
+    // fn setup_schema(db: &ManagedConnection) -> Result<sqlite::ResultCode, sqlite::ResultCode> {
+    //     db.exec_safe("CREATE TABLE foo (id INTEGER PRIMARY KEY);")?;
+    //     db.exec_safe("SELECT crsql_as_crr('foo');")
+    // }
 
-    setup_schema(&db_a)?;
-    setup_schema(&db_b)?;
+    // setup_schema(&db_a)?;
+    // setup_schema(&db_b)?;
 
-    let stmt = db_a.prepare_v2("INSERT INTO foo (id) VALUES (?);")?;
-    stmt.bind_int(1, 1)?;
-    stmt.step()?;
+    // let stmt = db_a.prepare_v2("INSERT INTO foo (id) VALUES (?);")?;
+    // stmt.bind_int(1, 1)?;
+    // stmt.step()?;
 
-    closedb(db_a)?;
-    closedb(db_b)?;
+    // closedb(db_a)?;
+    // closedb(db_b)?;
     Ok(())
 }

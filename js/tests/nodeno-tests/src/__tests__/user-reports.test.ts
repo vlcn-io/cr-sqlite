03945c1cf26d0da6118b9aca9af49fa2717f919e
Matt <1009003+tantaman@users.noreply.github.com>
2023-03-03 16:37:31 -0500

    expect output in user-reports.test.ts

--- js/tests/nodeno-tests/src/__tests__/user-reports.test.ts
+++ js/tests/nodeno-tests/src/__tests__/user-reports.test.ts
@@ -7,6 +7,20 @@ test("pk only table", () => {
   db.exec(`CREATE TABLE IF NOT EXISTS data (id NUMBER PRIMARY KEY)`);
   db.exec(`SELECT crsql_as_crr('data')`);
   db.exec(`INSERT INTO data VALUES (42) ON CONFLICT DO NOTHING`);
-  console.log(db.prepare(`SELECT * FROM data`).all());
-  console.log(db.prepare(`SELECT * FROM crsql_changes`).all());
+  expect(
+    db
+      .prepare(
+        `SELECT "table", "pk", "cid", "val", "col_version", "db_version" FROM crsql_changes`
+      )
+      .all()
+  ).toEqual([
+    {
+      table: "data",
+      pk: "42",
+      cid: "__crsql_pko",
+      val: null,
+      col_version: 1,
+      db_version: 1,
+    },
+  ]);
 });

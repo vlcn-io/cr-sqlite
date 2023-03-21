e59866e92b9597ed64bf655e37b95cea81841e5d
Matt <1009003+tantaman@users.noreply.github.com>
2023-03-21 16:17:41 -0400

    make return values ffi safe, move tear down code into rust, move extern c code to appropriate modules

--- core/rs/core/src/lib.rs
+++ core/rs/core/src/lib.rs
@@ -14,5 +14,109 @@
 #![cfg_attr(not(test), no_std)]
 
 mod backfill;
+mod teardown;
 
+use core::{ffi::c_char, slice};
+extern crate alloc;
+use alloc::format;
+use alloc::string::String;
+use alloc::vec::Vec;
 pub use backfill::*;
+use core::ffi::{c_int, CStr};
+use sqlite::ResultCode;
+use sqlite_nostd as sqlite;
+use sqlite_nostd::{context, Connection, Context, Value};
+pub use teardown::*;
+
+fn escape_ident(ident: &str) -> String {
+    return ident.replace("\"", "\"\"");
+}
+
+pub extern "C" fn crsql_as_table(
+    ctx: *mut sqlite::context,
+    argc: i32,
+    argv: *mut *mut sqlite::value,
+) {
+    let args = sqlite::args!(argc, argv);
+    let db = ctx.db_handle();
+    let table = args[0].text();
+    if let Err(_) = remove_crr_clock_table_if_exists(db, table) {
+        ctx.result_error(&format!("could not downgrade CRR {table}", table = table))
+    }
+}
+
+#[no_mangle]
+pub extern "C" fn sqlite3_crsqlcore_init(
+    db: *mut sqlite::sqlite3,
+    _err_msg: *mut *mut c_char,
+    api: *mut sqlite::api_routines,
+) -> c_int {
+    sqlite::EXTENSION_INIT2(api);
+
+    db.create_function_v2(
+        "crsql_as_table",
+        1,
+        sqlite::UTF8,
+        None,
+        Some(crsql_as_table),
+        None,
+        None,
+        None,
+    )
+    .unwrap_or(sqlite::ResultCode::ERROR) as c_int
+}
+
+#[no_mangle]
+pub extern "C" fn crsql_backfill_table(
+    context: *mut context,
+    table: *const c_char,
+    pk_cols: *const *const c_char,
+    pk_cols_len: c_int,
+    non_pk_cols: *const *const c_char,
+    non_pk_cols_len: c_int,
+) -> c_int {
+    let table = unsafe { CStr::from_ptr(table).to_str() };
+    let pk_cols = unsafe {
+        let parts = slice::from_raw_parts(pk_cols, pk_cols_len as usize);
+        parts
+            .iter()
+            .map(|&p| CStr::from_ptr(p).to_str())
+            .collect::<Result<Vec<_>, _>>()
+    };
+    let non_pk_cols = unsafe {
+        let parts = slice::from_raw_parts(non_pk_cols, non_pk_cols_len as usize);
+        parts
+            .iter()
+            .map(|&p| CStr::from_ptr(p).to_str())
+            .collect::<Result<Vec<_>, _>>()
+    };
+
+    let result = match (table, pk_cols, non_pk_cols) {
+        (Ok(table), Ok(pk_cols), Ok(non_pk_cols)) => {
+            let db = context.db_handle();
+            backfill_table(db, table, pk_cols, non_pk_cols)
+        }
+        _ => Err(ResultCode::ERROR),
+    };
+
+    match result {
+        Ok(result) => result as c_int,
+        Err(result) => result as c_int,
+    }
+}
+
+#[no_mangle]
+pub extern "C" fn crsql_remove_crr_triggers_if_exist(
+    db: *mut sqlite::sqlite3,
+    table: *const c_char,
+) -> c_int {
+    if let Ok(table) = unsafe { CStr::from_ptr(table).to_str() } {
+        let result = remove_crr_triggers_if_exist(db, table);
+        match result {
+            Ok(result) => result as c_int,
+            Err(result) => result as c_int,
+        }
+    } else {
+        ResultCode::NOMEM as c_int
+    }
+}
---
+++ core/rs/core/src/teardown.rs
@@ -0,0 +1,51 @@
+/*
+ * Copyright 2023 One Law LLC. All Rights Reserved.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+use sqlite_nostd as sqlite;
+use sqlite_nostd::{Connection, ResultCode};
+extern crate alloc;
+use alloc::format;
+
+pub fn remove_crr_clock_table_if_exists(
+    db: *mut sqlite::sqlite3,
+    table: &str,
+) -> Result<ResultCode, ResultCode> {
+    let escaped_table = crate::escape_ident(table);
+    // Dropping a table drops all triggers automagically
+    db.exec_safe(&format!(
+        "DROP TABLE IF EXISTS \"{table}\"",
+        table = escaped_table
+    ))
+}
+
+pub fn remove_crr_triggers_if_exist(
+    db: *mut sqlite::sqlite3,
+    table: &str,
+) -> Result<ResultCode, ResultCode> {
+    let escaped_table = crate::escape_ident(table);
+
+    db.exec_safe(&format!(
+        "DROP TRIGGER IF EXISTS \"{table}__crsql_itrig\"",
+        table = escaped_table
+    ))?;
+
+    db.exec_safe(&format!(
+        "DROP TRIGGER IF EXISTS \"{table}__crsql_utrig\"",
+        table = escaped_table
+    ))?;
+
+    db.exec_safe(&format!(
+        "DROP TRIGGER IF EXISTS \"{table}__crsql_dtrig\"",
+        table = escaped_table
+    ))
+}

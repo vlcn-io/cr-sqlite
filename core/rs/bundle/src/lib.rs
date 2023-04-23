117b815ffe3239c0ac3fcbffed08c0a8f54a76b8
Matt <1009003+tantaman@users.noreply.github.com>
2023-04-23 13:44:09 -0400

    alloc_error_handler has stabilized
    https://github.com/rust-lang/rust/issues/51540

--- core/rs/bundle/src/lib.rs
+++ core/rs/bundle/src/lib.rs
@@ -1,6 +1,5 @@
 #![no_std]
 #![feature(core_intrinsics)]
-#![feature(alloc_error_handler)]
 #![feature(lang_items)]
 
 extern crate alloc;
@@ -20,11 +19,6 @@ static ALLOCATOR: SQLite3Allocator = SQLite3Allocator {};
 
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
-    core::intrinsics::abort()
-}
-
-#[alloc_error_handler]
-fn oom(_: Layout) -> ! {
     core::intrinsics::abort()
 }
 

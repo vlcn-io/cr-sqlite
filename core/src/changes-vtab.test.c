27abe1e86b6046c538e88bdbb19ade46cc974861
Matt <1009003+tantaman@users.noreply.github.com>
2023-04-24 09:23:24 -0400

    add some tests for the new filter handling

--- core/src/changes-vtab.test.c
+++ core/src/changes-vtab.test.c
@@ -25,7 +25,7 @@ static void testManyPkTable() {
   rc += sqlite3_exec(db, "INSERT INTO foo VALUES (4,5,6);", 0, 0, 0);
   assert(rc == SQLITE_OK);
 
-  rc += sqlite3_prepare_v2(db, "SELECT * FROM crsql_changes()", -1, &pStmt, 0);
+  rc += sqlite3_prepare_v2(db, "SELECT * FROM crsql_changes", -1, &pStmt, 0);
   assert(rc == SQLITE_OK);
 
   while (sqlite3_step(pStmt) == SQLITE_ROW) {
@@ -37,6 +37,77 @@ static void testManyPkTable() {
   crsql_close(db);
   printf("\t\e[0;32mSuccess\e[0m\n");
 }
+
+static void assertCount(sqlite3 *db, const char *sql, int expected) {
+  sqlite3_stmt *pStmt;
+  int rc = sqlite3_prepare_v2(db, sql, -1, &pStmt, 0);
+  assert(rc == SQLITE_OK);
+  assert(sqlite3_step(pStmt) == SQLITE_ROW);
+  printf("expected: %d, actual: %d\n", expected, sqlite3_column_int(pStmt, 0));
+  assert(sqlite3_column_int(pStmt, 0) == expected);
+  sqlite3_finalize(pStmt);
+}
+
+static void testFilters() {
+  printf("Filters\n");
+
+  sqlite3 *db;
+  int rc;
+  rc = sqlite3_open(":memory:", &db);
+
+  rc = sqlite3_exec(db, "CREATE TABLE foo (a primary key, b);", 0, 0, 0);
+  rc += sqlite3_exec(db, "SELECT crsql_as_crr('foo');", 0, 0, 0);
+  assert(rc == SQLITE_OK);
+  rc += sqlite3_exec(db, "INSERT INTO foo VALUES (1,2);", 0, 0, 0);
+  rc += sqlite3_exec(db, "INSERT INTO foo VALUES (2,3);", 0, 0, 0);
+  rc += sqlite3_exec(db, "INSERT INTO foo VALUES (3,4);", 0, 0, 0);
+  assert(rc == SQLITE_OK);
+
+  printf("no filters\n");
+  assertCount(db, "SELECT count(*) FROM crsql_changes", 3);
+
+  // now test:
+  // 1. site_id comparison
+  // 2. db_version comparison
+
+  printf("is null\n");
+  assertCount(db, "SELECT count(*) FROM crsql_changes WHERE site_id IS NULL",
+              3);
+
+  printf("is not null\n");
+  assertCount(
+      db, "SELECT count(*) FROM crsql_changes WHERE site_id IS NOT NULL", 0);
+
+  printf("equals\n");
+  assertCount(
+      db, "SELECT count(*) FROM crsql_changes WHERE site_id = crsql_siteid()",
+      0);
+
+  // 0 rows is actually correct ANSI sql behavior. NULLs are never equal, or not
+  // equal, to anything in ANSI SQL. So users must use `IS NOT` to check rather
+  // than !=.
+  // https://stackoverflow.com/questions/60017275/why-null-is-not-equal-to-anything-is-a-false-statement
+  printf("not equals\n");
+  assertCount(
+      db, "SELECT count(*) FROM crsql_changes WHERE site_id != crsql_siteid()",
+      0);
+
+  printf("is not\n");
+  // All rows are currently null for site_id
+  assertCount(
+      db,
+      "SELECT count(*) FROM crsql_changes WHERE site_id IS NOT crsql_siteid()",
+      3);
+
+  // compare on db_version _and_ site_id
+
+  // compare on pks, table name, other not perfectly supported columns
+
+  crsql_close(db);
+  printf("\t\e[0;32mSuccess\e[0m\n");
+}
+
+// test value extraction under all filter conditions
 
 // static void testSinglePksTable()
 // {
@@ -57,4 +128,5 @@ static void testManyPkTable() {
 void crsqlChangesVtabTestSuite() {
   printf("\e[47m\e[1;30mSuite: crsql_changesVtab\e[0m\n");
   testManyPkTable();
+  testFilters();
 }

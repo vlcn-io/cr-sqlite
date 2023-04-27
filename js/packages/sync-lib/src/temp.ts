667fc106f199652bbff79524a8d2b17c544501f0
Matt <1009003+tantaman@users.noreply.github.com>
2023-04-27 11:42:08 -0400

    awful workaround for macos bug in fsnotify
    We'll abstract away the "touching" into something debounced over ~50ms and within the `applyChangeset` method so we never forget to touch.
    
    https://github.com/emcrisostomo/fswatch/issues/150

--- js/packages/sync-lib/src/temp.ts
+++ js/packages/sync-lib/src/temp.ts
@@ -1,16 +1,16 @@
-import DBCache from "./DBCache.js";
-import TestConfig from "./TestConfig.js";
-import { createFsNotify } from "./private/FSNotify.js";
+// import DBCache from "./DBCache.js";
+// import TestConfig from "./TestConfig.js";
+// import { createFsNotify } from "./private/FSNotify.js";
 
-async function start() {
-  const cache = new DBCache(TestConfig);
-  await createFsNotify(TestConfig, cache);
-  console.log("sleeping");
-  await sleep(1000000);
-}
+// async function start() {
+//   const cache = new DBCache(TestConfig);
+//   await createFsNotify(TestConfig, cache);
+//   console.log("sleeping");
+//   await sleep(1000000);
+// }
 
-async function sleep(ms: number) {
-  return new Promise((resolve) => setTimeout(resolve, ms));
-}
+// async function sleep(ms: number) {
+//   return new Promise((resolve) => setTimeout(resolve, ms));
+// }
 
-start();
+// start();

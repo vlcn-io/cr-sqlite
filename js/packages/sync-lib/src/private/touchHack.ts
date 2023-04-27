5b2bb39ff216b7cf7ad6ae4bf3e83a33a9b01236
Matt <1009003+tantaman@users.noreply.github.com>
2023-04-27 12:14:27 -0400

    throttled touch hack

---
+++ js/packages/sync-lib/src/private/touchHack.ts
@@ -0,0 +1,15 @@
+import { Config } from "../Types.js";
+import { throttle } from "throttle-debounce";
+import util from "../util.js";
+
+const apply = throttle(50, (config: Config, dbid: string) => {
+  util.touchFile(config, dbid);
+});
+
+export default function touchHack(config: Config, dbid: string) {
+  if (!util.isDarwin()) {
+    return;
+  }
+
+  apply(config, dbid);
+}

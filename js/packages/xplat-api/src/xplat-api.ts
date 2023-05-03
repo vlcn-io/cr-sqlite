228e4fc89fec67e418c0b7a486838aa8198fdf2c
Matt <1009003+tantaman@users.noreply.github.com>
2023-05-03 11:51:29 -0400

    expose an auto-migrate method in the wasm api

--- js/packages/xplat-api/src/xplat-api.ts
+++ js/packages/xplat-api/src/xplat-api.ts
@@ -87,3 +87,39 @@ export interface StmtAsync {
 }
 
 export const version = 1;
+
+export function cryb64(str: string, seed: number = 0) {
+  let h1 = 0xdeadbeef ^ seed,
+    h2 = 0x41c6ce57 ^ seed;
+  for (let i = 0, ch; i < str.length; i++) {
+    ch = str.charCodeAt(i);
+    h1 = Math.imul(h1 ^ ch, 2654435761);
+    h2 = Math.imul(h2 ^ ch, 1597334677);
+  }
+  h1 = Math.imul(h1 ^ (h1 >>> 16), 2246822507);
+  h1 ^= Math.imul(h2 ^ (h2 >>> 13), 3266489909);
+  h2 = Math.imul(h2 ^ (h2 >>> 16), 2246822507);
+  h2 ^= Math.imul(h1 ^ (h1 >>> 13), 3266489909);
+
+  return 4294967296n * BigInt(h2) + BigInt(h1);
+}
+
+export function first<T>(data: T[]): T | undefined {
+  if (!data) {
+    return undefined;
+  }
+  return data[0];
+}
+
+export function firstPick<T>(data: any[]): T | undefined {
+  const d = data[0];
+  if (d == null) {
+    return undefined;
+  }
+
+  return d[Object.keys(d)[0]];
+}
+
+export function pick<T extends any, R>(data: T[]): R[] {
+  return data.map((d: any) => d[Object.keys(d)[0] as any]);
+}

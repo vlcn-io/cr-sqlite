358c997e7acdc6eaf23fea6523ebeafb631c3732
Matt <1009003+tantaman@users.noreply.github.com>
2023-05-10 09:23:16 -0500

    get cypress type definitions

--- js/packages/direct-connect-browser/cypress/component/worker-DB.cy.ts
+++ js/packages/direct-connect-browser/cypress/component/worker-DB.cy.ts
@@ -1,5 +1,11 @@
 describe("WholeDbReplicator.cy.ts", () => {
-  it("should work", () => {
+  it("constructs a db without error", () => {
     expect(true).to.equal(true);
   });
+
+  it("pulls changesets from the local db", () => {});
+
+  it("applies changesets", () => {});
+
+  it("can update the peer tracker", () => {});
 });

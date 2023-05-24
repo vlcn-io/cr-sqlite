a2ddbb6e4ce0f3c678a933f63b45c0e6d50f0d3d
Matt <1009003+tantaman@users.noreply.github.com>
2023-05-24 14:43:56 -0400

    remove packages that we being deprecated

--- js/packages/server-core/schemas/README.md
+++
@@ -1,1 +0,0 @@
-Place `.sqlite` files here to use as template schemas when creating a new database on demand.
--- js/packages/server-core/src/index.ts
+++
@@ -1,3 +0,0 @@
-export { Connection } from "./connection.js";
-export { default as logger } from "./logger.js";
-export { default as contextStore } from "./contextStore.js";

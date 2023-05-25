3f700d063f885b840ea6f2cc82c7f6700994edc0
Matt <1009003+tantaman@users.noreply.github.com>
2023-05-25 13:06:18 -0400

    keep referential equality if returned data from db does not change

--- scripts/update-pkgjson.sh
+++ scripts/update-pkgjson.sh
@@ -16,7 +16,7 @@ for pkg in "${pkgs[@]}"; do
   PACKAGE_EXISTS=$(jq -r --arg name "$NAME" '.dependencies | has($name)' package.json)
   if [ "$PACKAGE_EXISTS" = "true" ]; then
     if [[ $1 == "local" ]]; then
-      VERSION="local:$VERSION"
+      VERSION="link:$VERSION"
     fi
     # replace in package.json only if there is an entry for that package in the package.json
     jq --arg name "$NAME" --arg version "$VERSION" '.dependencies[$name] = $version' package.json > tmp.json && mv tmp.json package.json

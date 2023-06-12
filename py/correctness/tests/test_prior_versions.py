bbd05547ee7fbd592bdb84fb492d5f0839834bb9
Matt <1009003+tantaman@users.noreply.github.com>
2023-06-12 14:59:09 -0400

    add a v0.13.0 db for testing too

--- py/correctness/tests/test_prior_versions.py
+++ py/correctness/tests/test_prior_versions.py
@@ -11,3 +11,16 @@ def test_can_load_v0_12_0():
     rows = c.execute("SELECT *, seq FROM crsql_changes").fetchall()
     assert (rows == [('foo', "'one'", 'b', '2', 1, 1, None, 0),
                      ('bar', '1', 'b', '2', 1, 2, None, 0)])
+
+
+def test_can_load_v0_13_0():
+    prefix = "./prior-dbs/v0.13.0"
+    # copy the file given connecting might migrate it!
+    shutil.copyfile(prefix + ".prior-db", prefix + ".db")
+    c = connect(prefix + ".db")
+    rows = c.execute("SELECT *, seq FROM crsql_changes").fetchall()
+    assert (rows == [('foo', '1', 'b', '2', 1, 1, None, 0),
+                     ('foo', '3', 'b', '4', 1, 2, None, 0),
+                     ('foo', '5', 'b', '6', 1, 2, None, 1),
+                     ('foo', '6', 'b', '7', 1, 2, None, 2),
+                     ('foo', '8', 'b', '9', 1, 3, None, 0)])

0ddadc21f90b03b945817038587c00576816f6b2
Matt <1009003+tantaman@users.noreply.github.com>
2023-06-12 14:56:52 -0400

    test loading a db from the prior version

--- py/correctness/tests/test_prior_versions.py
+++ py/correctness/tests/test_prior_versions.py
@@ -8,5 +8,6 @@ def test_can_load_v0_12_0():
     # copy the file given connecting might migrate it!
     shutil.copyfile(prefix + ".prior-db", prefix + ".db")
     c = connect(prefix + ".db")
-    rows = c.execute("SELECT * FROM crsql_changes").fetchall()
-    pprint.pprint(rows)
+    rows = c.execute("SELECT *, seq FROM crsql_changes").fetchall()
+    assert (rows == [('foo', "'one'", 'b', '2', 1, 1, None, 0),
+                     ('bar', '1', 'b', '2', 1, 2, None, 0)])

34a5ecdcded9cb5e3749a4ebdeee4f64d90a3106
Matt <1009003+tantaman@users.noreply.github.com>
2023-06-13 16:29:02 -0400

    fixup trigger test

--- core/src/triggers.test.c
+++ core/src/triggers.test.c
@@ -67,21 +67,22 @@ static void testDeleteTriggerQuery() {
   rc += sqlite3_exec(db, "DROP TABLE foo", 0, 0, &errMsg);
 
   char *query = crsql_deleteTriggerQuery(tableInfo);
+  printf("Q: X%sX", query);
   assert(
       strcmp(
           "CREATE TRIGGER IF NOT EXISTS \"foo__crsql_dtrig\"      AFTER DELETE "
-          "ON \"foo\"    BEGIN      DELETE FROM \"foo__crsql_clock\" WHERE "
-          "crsql_internal_sync_bit() = 0 AND \"a\" = OLD.\"a\";            "
-          "INSERT INTO \"foo__crsql_clock\" (        \"a\",        "
-          "__crsql_col_name,        __crsql_col_version,        "
+          "ON \"foo\"    BEGIN      INSERT INTO \"foo__crsql_clock\" (        "
+          "\"a\",        __crsql_col_name,        __crsql_col_version,        "
           "__crsql_db_version,        __crsql_seq,        __crsql_site_id      "
           ") SELECT         OLD.\"a\",        '__crsql_del',        1,        "
           "crsql_nextdbversion(),        crsql_increment_and_get_seq(),        "
           "NULL      WHERE crsql_internal_sync_bit() = 0 ON CONFLICT DO UPDATE "
           "SET      __crsql_col_version = __crsql_col_version + 1,      "
           "__crsql_db_version = crsql_nextdbversion(),      __crsql_seq = "
-          "crsql_increment_and_get_seq(),      __crsql_site_id = NULL;      "
-          "END; ",
+          "crsql_increment_and_get_seq(),      __crsql_site_id = NULL;         "
+          "   DELETE FROM \"foo__crsql_clock\" WHERE crsql_internal_sync_bit() "
+          "= 0 AND \"a\" = OLD.\"a\" AND __crsql_col_name != '__crsql_del';    "
+          "  END; ",
           query) == 0);
 
   crsql_freeTableInfo(tableInfo);

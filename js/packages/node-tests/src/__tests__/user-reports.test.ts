d22c600d0f85ced8c1f6c08cda02f3aa9ad49cb6
Matt <1009003+tantaman@users.noreply.github.com>
2023-06-13 08:48:47 -0400

    IS NOT rather than !=

--- js/packages/node-tests/src/__tests__/user-reports.test.ts
+++ js/packages/node-tests/src/__tests__/user-reports.test.ts
@@ -1,5 +1,8 @@
 import { test, expect } from "vitest";
 import crsqlite from "@vlcn.io/crsqlite-allinone";
+import { extensionPath } from "@vlcn.io/crsqlite";
+// @ts-ignore
+import SQLite from "better-sqlite3";
 
 // https://discord.com/channels/989870439897653248/989870440585494530/1081084118680485938
 test("pk only table", () => {
@@ -26,4 +29,45 @@ test("pk only table", () => {
 });
 
 // https://discord.com/channels/989870439897653248/989870440585494530/1118123015364939836
-test("failed to increment?", () => {});
+test("failed to increment?", () => {
+  const database = new SQLite(":memory:");
+  database.loadExtension(extensionPath);
+
+  // db_version is incremented with initial other then 'NULL'
+  const initial = "NULL"; // "0";
+
+  database.exec(`
+    CREATE TABLE a(id PRIMARY KEY, data);
+    CREATE TABLE b(id PRIMARY KEY, data);
+
+    SELECT crsql_as_crr('a');
+    SELECT crsql_as_crr('b');
+
+    INSERT INTO b VALUES (0, ${initial});
+  `);
+
+  database.exec(`
+    INSERT INTO a VALUES (10, 123);
+    UPDATE b SET data = 10 WHERE data IS ${initial};
+  `);
+  expect(database.prepare(`SELECT * FROM crsql_changes`).all()).toEqual([
+    {
+      table: "a",
+      pk: "10",
+      cid: "data",
+      val: "123",
+      col_version: 1,
+      db_version: 2,
+      site_id: null,
+    },
+    {
+      table: "b",
+      pk: "0",
+      cid: "data",
+      val: "10",
+      col_version: 2,
+      db_version: 3,
+      site_id: null,
+    },
+  ]);
+});

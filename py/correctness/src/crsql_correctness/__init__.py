1c6764c8e301ec82c33ad84194bfc98f18ca8ff3
Matt <1009003+tantaman@users.noreply.github.com>
2023-06-23 10:07:13 -0700

    test opening a db as readonly

--- py/correctness/src/crsql_correctness/__init__.py
+++ py/correctness/src/crsql_correctness/__init__.py
@@ -1,15 +1,18 @@
 import sqlite3
 
 extension = '../../core/dist/crsqlite'
-def connect(db_file):
-  c = sqlite3.connect(db_file)
-  c.enable_load_extension(True)
-  c.load_extension(extension)
-  return c
+
+
+def connect(db_file, uri=False):
+    c = sqlite3.connect(db_file, uri=uri)
+    c.enable_load_extension(True)
+    c.load_extension(extension)
+    return c
+
 
 def close(c):
-  c.execute("select crsql_finalize()")
-  c.close()
+    c.execute("select crsql_finalize()")
+    c.close()
 
 
 min_db_v = 0

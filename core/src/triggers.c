0aad504e3e7ecf40567ab8a162ef19fd95fc8cb0
Matt <1009003+tantaman@users.noreply.github.com>
2023-07-16 14:02:26 -0400

    rip out c code for create_update_trigger

--- core/src/triggers.c
+++ core/src/triggers.c
@@ -7,114 +7,6 @@
 #include "tableinfo.h"
 #include "util.h"
 
-// TODO (#50): we need to handle the case where someone _changes_ a primary key
-// column's value we should:
-// 1. detect this
-// 2. treat _every_ column as updated
-// 3. write a delete sentinel against the _old_ pk combination
-//
-// 1 is moot.
-// 2 is done via changing trigger conditions to: `WHERE sync_bit = 0 AND (NEW.c
-// != OLD.c OR NEW.pk_c1 != OLD.pk_c1 OR NEW.pk_c2 != ...) 3 is done with a new
-// trigger based on only pks
-int crsql_createUpdateTrigger(sqlite3 *db, crsql_TableInfo *tableInfo,
-                              char **err) {
-  char *zSql;
-  char *pkList = 0;
-  char *pkNewList = 0;
-  int rc = SQLITE_OK;
-  const int length = tableInfo->nonPksLen == 0 ? 1 : tableInfo->nonPksLen;
-  char **subTriggers = sqlite3_malloc(length * sizeof(char *));
-  char *joinedSubTriggers;
-
-  pkList = crsql_asIdentifierList(tableInfo->pks, tableInfo->pksLen, 0);
-  pkNewList = crsql_asIdentifierList(tableInfo->pks, tableInfo->pksLen, "NEW.");
-
-  // If we updated a table that _only_ has primary key columns
-  // this is the same thing as
-  // a
-  // 1. delete of the old row
-  // followed by
-  // 2. create of a new row
-  //
-  // TODO: Do we not also need to record a creation event
-  // if a pk was changed for a non pk only table?
-  if (tableInfo->nonPksLen == 0) {
-    subTriggers[0] = sqlite3_mprintf(
-        "INSERT INTO \"%s__crsql_clock\" (\
-        %s,\
-        __crsql_col_name,\
-        __crsql_col_version,\
-        __crsql_db_version,\
-        __crsql_seq,\
-        __crsql_site_id\
-      ) SELECT \
-        %s,\
-        %Q,\
-        1,\
-        crsql_nextdbversion(),\
-        crsql_increment_and_get_seq(),\
-        NULL\
-      WHERE crsql_internal_sync_bit() = 0 ON CONFLICT DO UPDATE SET\
-        __crsql_col_version = __crsql_col_version + 1,\
-        __crsql_db_version = crsql_nextdbversion(),\
-        __crsql_seq = crsql_get_seq() - 1,\
-        __crsql_site_id = NULL;\n",
-        tableInfo->tblName, pkList, pkNewList, PKS_ONLY_CID_SENTINEL);
-  }
-
-  // all other triggers can remain the same since we still must compare value on
-  // update pk cols should get update of triggers, however, so we can invoke the
-  // additional delete or whatever logic.
-  // leverage when clauses? rather than sync bit where stuff.. as well as new !=
-  // old stuff.
-
-  for (int i = 0; i < tableInfo->nonPksLen; ++i) {
-    // updates are conditionally inserted on the new value not being
-    // the same as the old value.
-    subTriggers[i] = sqlite3_mprintf(
-        "INSERT INTO \"%s__crsql_clock\" (\
-        %s,\
-        __crsql_col_name,\
-        __crsql_col_version,\
-        __crsql_db_version,\
-        __crsql_seq,\
-        __crsql_site_id\
-      ) SELECT %s, %Q, 1, crsql_nextdbversion(), crsql_increment_and_get_seq(), NULL WHERE crsql_internal_sync_bit() = 0 AND NEW.\"%w\" IS NOT OLD.\"%w\"\
-      ON CONFLICT DO UPDATE SET\
-        __crsql_col_version = __crsql_col_version + 1,\
-        __crsql_db_version = crsql_nextdbversion(),\
-        __crsql_seq = crsql_get_seq() - 1,\
-        __crsql_site_id = NULL;\n",
-        tableInfo->tblName, pkList, pkNewList, tableInfo->nonPks[i].name,
-        tableInfo->nonPks[i].name, tableInfo->nonPks[i].name);
-  }
-  joinedSubTriggers = crsql_join(subTriggers, length);
-
-  for (int i = 0; i < length; ++i) {
-    sqlite3_free(subTriggers[i]);
-  }
-  sqlite3_free(subTriggers);
-
-  zSql = sqlite3_mprintf(
-      "CREATE TRIGGER IF NOT EXISTS \"%s__crsql_utrig\"\
-      AFTER UPDATE ON \"%s\"\
-    BEGIN\
-      %s\
-    END;",
-      tableInfo->tblName, tableInfo->tblName, joinedSubTriggers);
-
-  sqlite3_free(joinedSubTriggers);
-
-  rc = sqlite3_exec(db, zSql, 0, 0, err);
-  sqlite3_free(zSql);
-
-  sqlite3_free(pkList);
-  sqlite3_free(pkNewList);
-
-  return rc;
-}
-
 static char *compareWithOld(const char *in) {
   return sqlite3_mprintf("\"%w\" = OLD.\"%w\"", in, in);
 }
@@ -186,7 +78,7 @@ int crsql_createCrrTriggers(sqlite3 *db, crsql_TableInfo *tableInfo,
                             char **err) {
   int rc = crsql_create_insert_trigger(db, tableInfo, err);
   if (rc == SQLITE_OK) {
-    rc = crsql_createUpdateTrigger(db, tableInfo, err);
+    rc = crsql_create_update_trigger(db, tableInfo, err);
   }
   if (rc == SQLITE_OK) {
     rc = crsql_createDeleteTrigger(db, tableInfo, err);

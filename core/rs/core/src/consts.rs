0dd260cbadd20edd4bb63b95749f48a79209992b
Matt <1009003+tantaman@users.noreply.github.com>
2023-07-17 15:56:50 -0400

    impl update and crsql_create_clock_table

--- core/rs/core/src/consts.rs
+++ core/rs/core/src/consts.rs
@@ -1,1 +1,5 @@
 pub const TBL_SITE_ID: &'static str = "__crsql_siteid";
+pub const TBL_SCHEMA: &'static str = "crsql_master";
+pub const CLOCK_TABLES_SELECT: &'static str =
+    "SELECT tbl_name FROM sqlite_master WHERE type='table' AND tbl_name LIKE '%__crsql_clock'";
+pub const CRSQLITE_VERSION: i32 = 130000;

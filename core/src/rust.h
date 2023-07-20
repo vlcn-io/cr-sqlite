261f9a12e2d69d44c98204f80720357db39ac946
Matt <1009003+tantaman@users.noreply.github.com>
2023-07-19 23:48:32 -0400

    remove dead code

--- core/src/rust.h
+++ core/src/rust.h
@@ -8,24 +8,12 @@
 // structures to the old C-code that hasn't been converted yet.
 // These are those definitions.
 
-typedef struct RawVec RawVec;
-struct RawVec {
-  void *ptr;
-  int len;
-  int cap;
-};
-
-RawVec crsql_unpack_columns(sqlite3_value *value);
-
-int crsql_bind_unpacked_values(sqlite3_stmt *stmt, RawVec columns);
-
 int crsql_backfill_table(sqlite3_context *context, const char *tblName,
                          const char **zpkNames, int pkCount,
                          const char **zNonPkNames, int nonPkCount,
                          int isCommitAlter);
 int crsql_is_crr(sqlite3 *db, const char *tblName);
 int crsql_compare_sqlite_values(const sqlite3_value *l, const sqlite3_value *r);
-void crsql_free_unpacked_values(RawVec columns);
 int crsql_create_crr_triggers(sqlite3 *db, crsql_TableInfo *tableInfo,
                               char **err);
 int crsql_remove_crr_triggers_if_exist(sqlite3 *db, const char *tblName);
@@ -39,14 +27,5 @@ int crsql_init_site_id(sqlite3 *db, unsigned char *ret);
 int crsql_init_peer_tracking_table(sqlite3 *db);
 int crsql_create_schema_table_if_not_exists(sqlite3 *db);
 int crsql_maybe_update_db(sqlite3 *db);
-
-#define TBL 0
-#define PKS 1
-#define CID 2
-#define COL_VRSN 3
-#define DB_VRSN 4
-#define SITE_ID 5
-#define CHANGES_ROWID 6
-#define SEQ 7
 
 #endif

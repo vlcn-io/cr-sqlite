e0c17577fc0e303ddfb29bc92fd73fda3b911e33
Matt <1009003+tantaman@users.noreply.github.com>
2023-07-26 15:54:47 -0400

    fixup testing of loading prior versions of dbs persisted with crsqlite

--- py/correctness/tests/test_prior_versions.py
+++ py/correctness/tests/test_prior_versions.py
@@ -9,13 +9,14 @@ def test_can_load_v0_12_0():
     shutil.copyfile(prefix + ".prior-db", prefix + ".db")
     c = connect(prefix + ".db")
     rows = c.execute("SELECT *, seq FROM crsql_changes").fetchall()
-    pprint(rows)
-    assert (rows == [('foo', b'\x01\x0b\x03one', 'b', 2, 1, 1, None, 1, 0),
-                     ('bar', b'\x01\x09\x01', 'b', 2, 1, 2, None, 1, 0)])
+    assert (rows == [('foo', b'\x01\x0b\x03one', '-1', None, 1, 0, None, 1, 0),
+                     ('bar', b'\x01\t\x01', '-1', None, 1, 0, None, 1, 0),
+                     ('foo', b'\x01\x0b\x03one', 'b', 2, 1, 1, None, 1, 0),
+                     ('bar', b'\x01\t\x01', 'b', 2, 1, 2, None, 1, 0)])
 
     version = c.execute(
         "SELECT value FROM crsql_master WHERE key ='crsqlite_version'").fetchone()
-    assert (version[0] == 130000)
+    assert (version[0] == 150000)
     close(c)
 
 
@@ -25,20 +26,25 @@ def test_can_load_v0_13_0():
     shutil.copyfile(prefix + ".prior-db", prefix + ".db")
     c = connect(prefix + ".db")
     rows = c.execute("SELECT *, seq FROM crsql_changes").fetchall()
-    assert (rows == [('foo', b'\x01\x09\x01', 'b', 2, 1, 1, None, 1, 0),
-                     ('foo', b'\x01\x09\x03', 'b', 4, 1, 2, None, 1, 0),
-                     ('foo', b'\x01\x09\x05', 'b', 6, 1, 2, None, 1, 1),
-                     ('foo', b'\x01\x09\x06', 'b', 7, 1, 2, None, 1, 2),
-                     ('foo', b'\x01\x09\x08', 'b', 9, 1, 3, None, 1, 0)])
+    assert (rows == [('foo', b'\x01\t\x01', '-1', None, 1, 0, None, 1, 0),
+                     ('foo', b'\x01\t\x03', '-1', None, 1, 0, None, 1, 0),
+                     ('foo', b'\x01\t\x05', '-1', None, 1, 0, None, 1, 0),
+                     ('foo', b'\x01\t\x06', '-1', None, 1, 0, None, 1, 0),
+                     ('foo', b'\x01\t\x08', '-1', None, 1, 0, None, 1, 0),
+                     ('foo', b'\x01\t\x01', 'b', 2, 1, 1, None, 1, 0),
+                     ('foo', b'\x01\t\x03', 'b', 4, 1, 2, None, 1, 0),
+                     ('foo', b'\x01\t\x05', 'b', 6, 1, 2, None, 1, 1),
+                     ('foo', b'\x01\t\x06', 'b', 7, 1, 2, None, 1, 2),
+                     ('foo', b'\x01\t\x08', 'b', 9, 1, 3, None, 1, 0)])
 
     version = c.execute(
         "SELECT value FROM crsql_master WHERE key ='crsqlite_version'").fetchone()
-    assert (version[0] == 130000)
+    assert (version[0] == 150000)
     close(c)
 
 
 def test_can_load_as_readonly():
-    prefix = "./prior-dbs/v0.13.0"
+    prefix = "./prior-dbs/v0.15.0"
     # copy the file given connecting might migrate it!
     shutil.copyfile(prefix + ".prior-db", prefix + ".db")
     c = connect('file:' + prefix + ".db?mode=ro", uri=True)

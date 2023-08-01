6b7e0423627a8848575da272fa0d7618b047a1ab
Matt <1009003+tantaman@users.noreply.github.com>
2023-08-01 14:00:07 -0400

    test more merge conditions for causal length

--- py/correctness/tests/test_cl_merging.py
+++ py/correctness/tests/test_cl_merging.py
@@ -101,15 +101,119 @@ def test_larger_cl_delete_deletes_all():
 
 
 def test_smaller_delete_does_not_delete_larger_cl():
-    None
+    c1 = make_simple_schema()
+    c2 = make_simple_schema()
+
+    c1.execute("INSERT INTO foo VALUES (1, 1)")
+    c1.execute("DELETE FROM foo")
+    c1.commit()
+
+    c2.execute("INSERT INTO foo VALUES (1, 1)")
+    c2.execute("DELETE FROM foo")
+    c2.execute("INSERT INTO foo VALUES (1, 1)")
+    c2.commit()
+
+    # check the pre-condition the c1 actually has a delete event
+    c1_changes = c1.execute("SELECT * FROM crsql_changes").fetchall()
+    assert (c1_changes == [('foo', b'\x01\t\x01', '-1', None, 2, 1, None, 2)])
+
+    c2_changes_pre_merge = c2.execute("SELECT * FROM crsql_changes").fetchall()
+
+    sync_left_to_right(c1, c2, 0)
+
+    c2_changes_post_merge = c2.execute(
+        "SELECT * FROM crsql_changes").fetchall()
+
+    # the merge should be a no-op since c1 can't impact c2 due to having a lesser causal length
+    # this the changesets should not change
+    assert (c2_changes_pre_merge == c2_changes_post_merge)
+
+
+def test_equivalent_delete_cls_is_noop():
+    c1 = make_simple_schema()
+    c2 = make_simple_schema()
+
+    c1.execute("INSERT INTO foo VALUES (1, 1)")
+    c1.execute("DELETE FROM foo")
+    c1.commit()
+
+    c2.execute("INSERT INTO foo VALUES (1, 1)")
+    c2.execute("DELETE FROM foo")
+    c2.commit()
+
+    # create a manual clock entry that wouldn't normally exist
+    # this clock entry would be removed if the merge does any work rather than bailing early
+    c2.execute(
+        "INSERT INTO foo__crsql_clock VALUES (1, 'b', 3, 1, NULL, 1)")
+    c2.commit()
+    pre_changes = c2.execute("SELECT * FROM crsql_changes").fetchall()
+    sync_left_to_right(c1, c2, 0)
+    post_changes = c2.execute("SELECT * FROM crsql_changes").fetchall()
+    assert (pre_changes == post_changes)
 
 
 def test_smaller_cl_loses_all():
-    None
+    c1 = make_simple_schema()
+    c2 = make_simple_schema()
+
+    c1.execute("INSERT INTO foo VALUES (1, 1)")
+    c1.execute("UPDATE foo SET b = 123 WHERE a = 1")
+    c1.commit()
+
+    c2.execute("INSERT INTO foo VALUES (1, 1)")
+    c2.execute("DELETE FROM foo")
+    c2.execute("INSERT INTO foo VALUES (1, 1)")
+    c2.commit()
+
+    pre_changes = c2.execute("SELECT * FROM crsql_changes").fetchall()
+    sync_left_to_right(c1, c2, 0)
+    post_changes = c2.execute("SELECT * FROM crsql_changes").fetchall()
+
+    # the merge should be a no-op since c1 has a lower causal length.
+    assert (pre_changes == post_changes)
 
 
 def test_pr_299_scenario():
     # https://github.com/vlcn-io/cr-sqlite/pull/299#issuecomment-1660570099
+    c1 = make_simple_schema()
+    c2 = make_simple_schema()
+
+    # c1 version 1 -- initial create
+    c1.execute("INSERT INTO foo VALUES (1, 1)")
+    c1.commit()
+
+    # c2 version 1 -- initial create and high clock values
+    c2.execute("INSERT INTO foo VALUES (1, 1)")
+    c2.execute("UPDATE foo SET b = 2 WHERE a = 1")
+    c2.execute("UPDATE foo SET b = 3 WHERE a = 1")
+    c2.execute("UPDATE foo SET b = 4 WHERE a = 1")
+    c2.commit()
+
+    # c1 version 2 -- delete
+    c1.execute("DELETE FROM foo")
+    c1.commit()
+
+    # c1 version 3 -- resurrect
+    c1.execute("INSERT INTO foo VALUES (1, 1)")
+
+    # send resurrect to c2, skip over earlier events and delete event
+    sync_left_to_right(c1, c2, 2)
+
+    changes = c2.execute("SELECT * FROM crsql_changes").fetchall()
+
+    # c2 should have accepted all the changes given the higher causal length
+    # a = 1, b = 1, cl = 3
+    # note: why is site_id missing??? sync left to right should have picked it up and inserted it...
+    assert (changes == [('foo', b'\x01\t\x01', '-1', None, 3, 3, None, 3),
+                        ('foo', b'\x01\t\x01', 'b', 1, 1, 3, None, 3)])
+    # c2 and c1 should match in terms of data
+    assert (c1.execute("SELECT * FROM foo").fetchall() ==
+            c2.execute("SELECT * FROM foo").fetchall())
+    # syncing the rest of c1 to c2 is a no-op
+    sync_left_to_right(c1, c2, 0)
+    post_changes = c2.execute("SELECT * FROM crsql_changes").fetchall()
+    assert (changes == post_changes)
+
     None
 
 
@@ -164,7 +268,7 @@ def test_cl_does_not_move_forward_when_equal():
     None
 
 
-# create a bunch of changes and merge them in random orders as driven by hypothesis.
+# create a bunch of changes and merge them in random orders (one item at a time) as driven by hypothesis.
 # end results should always be the same.
 def test_out_of_order_merge():
     None

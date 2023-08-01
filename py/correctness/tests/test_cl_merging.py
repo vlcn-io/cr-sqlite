380d98c43ea280d69c16fdf9951e86feeb3a819b
Matt <1009003+tantaman@users.noreply.github.com>
2023-08-01 11:51:35 -0400

    fix re-insertion algorithm to be commutative

--- py/correctness/tests/test_cl_merging.py
+++ py/correctness/tests/test_cl_merging.py
@@ -1,3 +1,7 @@
+from crsql_correctness import connect, close, min_db_v
+from pprint import pprint
+import pytest
+
 # - larger cl wins
 # - delete is deleted
 # - resurrect is resurrected
@@ -9,3 +13,115 @@
 #   - prop test with pko table
 #   - prop test with many tables
 #   - prop test with out-of-order sync
+
+
+def make_simple_schema():
+    c = connect(":memory:")
+    c.execute("CREATE TABLE foo (a INTEGER PRIMARY KEY, b INTEGER) STRICT;")
+    c.execute("SELECT crsql_as_crr('foo')")
+    c.commit()
+    return c
+
+
+def sync_left_to_right(l, r, since):
+    changes = l.execute(
+        "SELECT * FROM crsql_changes WHERE db_version > ?", (since,))
+    for change in changes:
+        r.execute(
+            "INSERT INTO crsql_changes VALUES (?, ?, ?, ?, ?, ?, ?, ?)", change)
+    r.commit()
+
+
+def test_larger_cl_wins_all():
+    c1 = make_simple_schema()
+    c2 = make_simple_schema()
+
+    c1.execute("INSERT INTO foo VALUES (1, 1)")
+    c1.execute("DELETE FROM foo")
+    c1.execute("INSERT INTO foo VALUES (1, 1)")
+    c1.commit()
+
+    c2.execute("INSERT INTO foo VALUES (1, 1)")
+    c2.execute("UPDATE foo SET b = 3 WHERE a = 1")
+    c2.execute("UPDATE foo SET b = 4 WHERE a = 1")
+    c2.commit()
+
+    # c2 has larger col versions for b (two updates) and larger values
+    # but has smaller causal length
+
+    # check assumed invariants
+    # c1 had a delete and resurrect so cl is 3
+    # and given metadata is cleared on delete, b is col_version 1
+    assert (c1.execute(
+        "SELECT col_version, cl FROM crsql_changes WHERE cid = 'b'").fetchone() == (1, 3))
+
+    # c2 hard col_version = 3 given insert + 2 updates
+    # an cl = 1 given a single isnert
+    assert (c2.execute(
+        "SELECT col_version, cl FROM crsql_changes WHERE cid = 'b'").fetchone() == (3, 1))
+
+    sync_left_to_right(c1, c2, 0)
+
+    # causal length moved forward so the winner column clock should be set to the
+    # insert column clock which is 1
+    assert (c2.execute(
+        "SELECT col_version, cl FROM crsql_changes WHERE cid = 'b'").fetchone() == (1, 3))
+
+    values = c2.execute("SELECT * FROM foo").fetchall()
+    # values in c2 are as expected -- the values from c1 since c1 has a later causal length
+    assert (values == [(1, 1)])
+
+    None
+
+
+def test_larger_cl_delete_delets_all():
+    None
+
+
+def test_resurrection_via_sentinel():
+    None
+
+
+def test_resurrection_via_non_sentinel():
+    None
+
+
+def test_delete_via_sentinel():
+    None
+
+
+def test_delete_via_non_sentinel():
+    None
+
+
+def test_strut_edits_out_of_order_merge():
+    None
+
+
+def test_strut_edits_in_order_merge():
+    None
+
+
+def test_larger_col_version_same_cl():
+    None
+
+
+# TODO: should we instead merge w/ site_id so we don't need a value lookup?
+def test_larger_col_value_same_cl_and_col_version():
+    None
+
+
+def test_pko_create():
+    None
+
+
+def test_pko_delete():
+    None
+
+
+def test_pko_resurrect():
+    None
+
+
+def test_cl_does_not_move_forward_when_equal():
+    None

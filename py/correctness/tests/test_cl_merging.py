8f92032a45df1164fabdd928ea1d3f7a5bbb5207
Matt <1009003+tantaman@users.noreply.github.com>
2023-07-31 21:25:56 -0400

    thoughts on next round of tests

--- py/correctness/tests/test_cl_merging.py
+++ py/correctness/tests/test_cl_merging.py
@@ -5,3 +5,7 @@
 # - cl not moved forward unless there is a delta on merge
 # - out of order for cl move up. non sentinel can resurrect or delete
 # - app with undo?
+# - update prop test to:
+#   - prop test with pko table
+#   - prop test with many tables
+#   - prop test with out-of-order sync

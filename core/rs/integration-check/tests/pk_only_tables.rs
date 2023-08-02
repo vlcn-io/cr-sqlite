036e33a24e0710d36ec062554d685e991accbfcb
Matt <1009003+tantaman@users.noreply.github.com>
2023-08-01 21:01:35 -0400

    fix rust tests -- actually sync causal length

--- core/rs/integration-check/tests/pk_only_tables.rs
+++ core/rs/integration-check/tests/pk_only_tables.rs
@@ -57,7 +57,7 @@ fn sync_left_to_right(
 
     while stmt_l.step()? == ResultCode::ROW {
         let stmt_r = r.prepare_v2("INSERT INTO crsql_changes VALUES (?, ?, ?, ?, ?, ?, ?, ?)")?;
-        for x in 0..7 {
+        for x in 0..8 {
             stmt_r.bind_value(x + 1, stmt_l.column_value(x)?)?;
         }
         stmt_r.step()?;

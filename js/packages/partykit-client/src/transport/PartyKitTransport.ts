9b32e58b7e6eb15c567ab873e3a6c83c5ad45092
Matt <1009003+tantaman@users.noreply.github.com>
2023-08-07 17:09:18 -0400

    bin encoding, implement transport interface for partykit

--- js/packages/partykit-client/src/transport/PartyKitTransport.ts
+++ js/packages/partykit-client/src/transport/PartyKitTransport.ts
@@ -1,4 +1,64 @@
-import PartySocket from "partysocket";
+import PartySocket, { PartySocketOptions } from "partysocket";
 import { Transport } from "./Transport";
+import {
+  AnnouncePresence,
+  Changes,
+  RejectChanges,
+  StartStreaming,
+  decode,
+  encode,
+  tags,
+} from "@vlcn.io/partykit-common";
 
-export default class PartyKitTransport implements Transport {}
+export default class PartyKitTransport implements Transport {
+  #socket;
+  #hadStartStream = false;
+
+  constructor(options: PartySocketOptions) {
+    this.#socket = new PartySocket(options);
+
+    this.#socket.addEventListener("message", this.#processEvent);
+  }
+
+  onChangesReceived: ((msg: Changes) => Promise<void>) | null = null;
+  onStartStreaming: ((msg: StartStreaming) => Promise<void>) | null = null;
+  onResetStream: ((msg: StartStreaming) => Promise<void>) | null = null;
+
+  #processEvent = (e: MessageEvent<any>) => {
+    const msg = decode(e.data);
+    switch (msg._tag) {
+      case tags.AnnouncePresence:
+      case tags.RejectChanges:
+        // clients should not receive these evnts
+        throw new Error(`Unexpected event: ${msg._tag}`);
+      case tags.Changes:
+        if (this.onChangesReceived) {
+          this.onChangesReceived(msg);
+        }
+        return;
+      case tags.StartStreaming:
+        if (this.#hadStartStream) {
+          this.onResetStream && this.onResetStream(msg);
+        } else {
+          this.#hadStartStream = true;
+          this.onStartStreaming && this.onStartStreaming(msg);
+        }
+    }
+  };
+
+  async announcePresence(msg: AnnouncePresence): Promise<void> {
+    this.#socket.send(encode(msg));
+  }
+
+  async sendChanges(msg: Changes): Promise<void> {
+    // TODO: hm... we can't see if it is still in-flight?
+    // or if we should back off on sending?
+    // Ideally we could observe the socket and back off on sending if we're sending too frequently for the
+    // receiver.
+    this.#socket.send(encode(msg));
+  }
+
+  async rejectChanges(msg: RejectChanges): Promise<void> {
+    this.#socket.send(encode(msg));
+  }
+}
---
+++ js/packages/partykit-common/src/decode.ts
@@ -0,0 +1,90 @@
+import * as decoding from "lib0/decoding";
+import {
+  AnnouncePresence,
+  Change,
+  Changes,
+  Msg,
+  RejectChanges,
+  StartStreaming,
+  tags,
+} from "./msgTypes.js";
+import { BIGINT, BLOB, BOOL, NULL, NUMBER, STRING } from "./encode";
+
+export function decode(msg: Uint8Array): Msg {
+  const decoder = decoding.createDecoder(msg);
+  const tag = decoding.readUint8(decoder);
+  switch (tag) {
+    case tags.AnnouncePresence:
+      return {
+        _tag: tags.AnnouncePresence,
+        sender: decoding.readUint8Array(decoder, 16),
+        lastSeens: Array.from({ length: decoding.readVarUint(decoder) }).map(
+          (_) => {
+            return [
+              decoding.readUint8Array(decoder, 16),
+              [decoding.readBigInt64(decoder), decoding.readVarInt(decoder)],
+            ];
+          }
+        ),
+        schemaVersion: decoding.readBigInt64(decoder),
+      } satisfies AnnouncePresence;
+    case tags.Changes:
+      return {
+        _tag: tags.Changes,
+        sender: decoding.readUint8Array(decoder, 16),
+        since: [decoding.readBigInt64(decoder), decoding.readVarInt(decoder)],
+        changes: readChanges(decoder),
+      } satisfies Changes;
+    case tags.RejectChanges:
+      return {
+        _tag: tags.RejectChanges,
+        since: [decoding.readBigInt64(decoder), decoding.readVarInt(decoder)],
+        whose: decoding.readUint8Array(decoder, 16),
+      } satisfies RejectChanges;
+    case tags.StartStreaming:
+      return {
+        _tag: tags.StartStreaming,
+        since: [decoding.readBigInt64(decoder), decoding.readVarInt(decoder)],
+        excludeSites: Array.from({ length: decoding.readVarUint(decoder) }).map(
+          (_) => {
+            return decoding.readUint8Array(decoder, 16);
+          }
+        ),
+        localOnly: decoding.readUint8(decoder) == 1 ? true : false,
+        schemaVersion: decoding.readBigInt64(decoder),
+      } satisfies StartStreaming;
+  }
+
+  throw new Error(`Unexpected tag: ${tag}`);
+}
+
+function readChanges(decoder: decoding.Decoder) {
+  return Array.from({ length: decoding.readVarUint(decoder) }, () => [
+    decoding.readVarString(decoder),
+    decoding.readVarUint8Array(decoder),
+    decoding.readVarString(decoder),
+    (() => {
+      const type = decoding.readUint8(decoder);
+      switch (type) {
+        case NULL:
+          return null;
+        case BIGINT:
+          return decoding.readBigInt64(decoder);
+        case NUMBER:
+          return decoding.readVarInt(decoder);
+        case STRING:
+          return decoding.readVarString(decoder);
+        case BOOL:
+          return decoding.readUint8(decoder) === 1 ? true : false;
+        case BLOB:
+          return decoding.readVarUint8Array(decoder);
+      }
+      throw new Error(`Unknown type ${type}`);
+    })(),
+    decoding.readBigInt64(decoder),
+    decoding.readBigInt64(decoder),
+    // TODO: we don't need to always pass site id
+    decoding.readUint8Array(decoder, 16),
+    decoding.readBigInt64(decoder),
+  ]) satisfies Change[];
+}

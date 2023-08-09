0865dd92d591b8b4e38bdfb63cb4b42750cd70ab
Matt <1009003+tantaman@users.noreply.github.com>
2023-08-09 19:59:36 -0400

    implement test to check that ordinals do not move after merging changes with existing site ids

--- py/correctness/tests/test_site_id_lookaside.py
+++ py/correctness/tests/test_site_id_lookaside.py
@@ -64,4 +64,37 @@ def test_local_changes_have_null_site():
 
 
 def test_site_id_ordinals_do_not_move_on_merge():
+    a = make_simple_schema()
+
+    a.execute(
+        "INSERT INTO crsql_changes VALUES ('foo', x'010901', 'b', 1, 1, 1, x'1dc8d6bb7f8941088327d9439a7927a4', 1)")
+    a.commit()
+
+    x = a.execute("SELECT quote(site_id) FROM crsql_changes").fetchall()
+    assert (x == [("X'1DC8D6BB7F8941088327D9439A7927A4'",)])
+
+    # insert again with the same site id
+    a.execute(
+        "INSERT INTO crsql_changes VALUES ('foo', x'010902', 'b', 1, 1, 1, x'1dc8d6bb7f8941088327d9439a7927a4', 1)")
+    a.commit()
+
+    x = a.execute("SELECT quote(site_id) FROM crsql_changes").fetchall()
+    # site ids should be returned with changes
+    assert (x == [("X'1DC8D6BB7F8941088327D9439A7927A4'",),
+                  ("X'1DC8D6BB7F8941088327D9439A7927A4'",)])
+
+    # insert a new site id
+    a.execute(
+        "INSERT INTO crsql_changes VALUES ('foo', x'010903', 'b', 1, 1, 1, x'2dc8d6bb7f8941088327d9439a7927a4', 1)")
+    a.commit()
+    # insert again with that new site id
+    a.execute(
+        "INSERT INTO crsql_changes VALUES ('foo', x'010904', 'b', 1, 1, 1, x'2dc8d6bb7f8941088327d9439a7927a4', 1)")
+    a.commit()
+    # should only be 2 site ids w/ ordinals 1 and 2. 1DC... -> 1, 2DC... -> 2
+    x = a.execute(
+        "SELECT quote(site_id), ordinal FROM crsql_site_id WHERE ordinal != 0").fetchall()
+    assert (x == [("X'1DC8D6BB7F8941088327D9439A7927A4'", 1),
+                  ("X'2DC8D6BB7F8941088327D9439A7927A4'", 2)])
+
     None

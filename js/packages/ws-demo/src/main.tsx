5b8ad44c062c41e0dcfcfc6d1d662785098d63e6
Matt <1009003+tantaman@users.noreply.github.com>
2023-08-09 12:42:02 -0400

    start wiring up the demo to use the new sync implementations

--- js/packages/ws-demo/src/main.tsx
+++ js/packages/ws-demo/src/main.tsx
@@ -1,1 +1,16 @@
+import { WorkerInterface } from "@vlcn.io/ws-client";
+import workerUrl from "@vlcn.io/ws-client/worker.js?url";
+import syncConfigUrl from "./syncConfig.js?url";
 console.log("hiya");
+
+const worker = new WorkerInterface(
+  syncConfigUrl,
+  import.meta.env.DEV ? workerUrl : undefined
+);
+
+worker.startSync("some-db", {
+  room: "some-room",
+  url: "http://localhost:8080/sync",
+});
+
+console.log("bya");

eab2937153974d32dd693bde924a6b012fc80aab
Matt <1009003+tantaman@users.noreply.github.com>
2023-08-10 10:59:08 -0400

    fix bug due to resetting auto-extension

--- js/packages/xplat-tests/src/automigrate.test.ts
+++ js/packages/xplat-tests/src/automigrate.test.ts
@@ -1,6 +1,7 @@
 import { DBAsync } from "@vlcn.io/xplat-api";
 type DB = DBAsync;
 
+// TODO: not xplat -- browser specific due to extra finalizae in automigrate
 export const tests = {
   "can automigrate an empty db": async (
     dbProvider: () => Promise<DB>,
@@ -9,11 +10,14 @@ export const tests = {
     const db = await dbProvider();
     try {
       db.tx(async (tx) => {
-        await tx.exec(`SELECT crsql_automigrate(?)`, [
-          `CREATE TABLE IF NOT EXISTS test (id PRIMARY KEY, name TEXT);
+        await tx.exec(
+          `SELECT crsql_automigrate(?, 'SELECT crsql_finalize()')`,
+          [
+            `CREATE TABLE IF NOT EXISTS test (id PRIMARY KEY, name TEXT);
   SELECT crsql_as_crr('test');
         `,
-        ]);
+          ]
+        );
       });
     } catch (e) {
       assert(false);

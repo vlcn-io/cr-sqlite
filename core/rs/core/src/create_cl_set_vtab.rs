f2735ecd6380edf112045db5d7567cee862d4f86
Matt <1009003+tantaman@users.noreply.github.com>
2023-08-14 11:55:56 -0400

    start implementing guts of clset vtab

--- core/rs/core/src/create_cl_set_vtab.rs
+++ core/rs/core/src/create_cl_set_vtab.rs
@@ -7,23 +7,79 @@ use alloc::boxed::Box;
 use alloc::ffi::CString;
 use alloc::format;
 use alloc::vec::Vec;
-use sqlite::{Connection, Context, Value};
+use sqlite::{Connection, Context, StrRef, VTabArgs, VTabRef, Value};
 use sqlite_nostd as sqlite;
 use sqlite_nostd::ResultCode;
 
 // Virtual table definition to create a causal length set backed table.
 
+// used in response to `create virtual table ... using clset`
 extern "C" fn create(
     db: *mut sqlite::sqlite3,
     _aux: *mut c_void,
-    _argc: c_int,
-    _argv: *const *const c_char,
+    argc: c_int,
+    argv: *const *const c_char,
     vtab: *mut *mut sqlite::vtab,
-    _err: *mut *mut c_char,
+    err: *mut *mut c_char,
 ) -> c_int {
-    0
+    let args = sqlite::parse_vtab_args(argc, argv);
+    match create_impl(db, argc, argv, vtab, err) {
+        Ok(rc) | Err(rc) => rc as c_int,
+    }
 }
 
+fn create_impl(
+    db: *mut sqlite::sqlite3,
+    argc: c_int,
+    argv: *const *const c_char,
+    vtab: *mut *mut sqlite::vtab,
+    err: *mut *mut c_char,
+) -> Result<ResultCode, ResultCode> {
+    // This is the schema component
+    sqlite::declare_vtab(
+        db,
+        "CREATE TABLE x(column_name TEXT, column_type TEXT, crdt_type TEXT);",
+    )?;
+    let tab = Box::new(sqlite::vtab {
+        nRef: 0,
+        pModule: core::ptr::null(),
+        zErrMsg: core::ptr::null_mut(),
+    });
+    vtab.set(tab);
+    sqlite::vtab_config(db, sqlite::INNOCUOUS);
+
+    // now we need to go and _create_ the backing storage / companion tables.
+    let vtab_args = sqlite::parse_vtab_args(argc, argv)?;
+    create_clset_storage(db, vtab_args, err)
+}
+
+fn create_clset_storage(
+    db: *mut sqlite::sqlite3,
+    args: VTabArgs,
+    err: *mut *mut c_char,
+) -> Result<ResultCode, ResultCode> {
+    // Is the _last_ arg all the args? Or is it comma separated in some way?
+    // What about index definitions...
+    // Let the user later create them against the base table? Or via insertions into our vtab schema?
+    let table_def = args.arguments.first();
+    if !args.table_name.ends_with("_schema") {
+        err.set("CLSet virtual table names must end with `_schema`");
+        return Err(ResultCode::MISUSE);
+    }
+    match table_def {
+        None => {
+            err.set("CLSet requires a table definition body.");
+            Err(ResultCode::MISUSE)
+        }
+        Some(table_def) => db.exec_safe(&format!(
+            "CREATE TABLE {table_name} ({table_def})",
+            table_name = &args.table_name[0..(args.table_name.len() - "_schema".len())],
+            table_def = table_def
+        )),
+    }
+}
+
+// connect to an existing virtual table previously created by `create virtual table`
 extern "C" fn connect(
     db: *mut sqlite::sqlite3,
     _aux: *mut c_void,
@@ -117,6 +173,8 @@ static MODULE: sqlite_nostd::module = sqlite_nostd::module {
 */
 pub fn create_module(db: *mut sqlite::sqlite3) -> Result<ResultCode, ResultCode> {
     db.create_module_v2("CLSet", &MODULE, None, None)?;
+
+    // xCreate(|x| 0);
 
     Ok(ResultCode::OK)
 }

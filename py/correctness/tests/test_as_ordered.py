4e39e602d6dbeb1ff414a01a42523258b3831d59
Matt <1009003+tantaman@users.noreply.github.com>
2023-08-21 12:42:07 -0400

    tests for fractional indexing as strut ran into issues

---
+++ py/correctness/tests/test_as_ordered.py
@@ -0,0 +1,34 @@
+from crsql_correctness import connect, close, min_db_v
+from pprint import pprint
+
+
+def test_first_insertion_start():
+    None
+
+
+def test_first_insertion_end():
+    None
+
+
+def test_middle_insertion():
+    None
+
+
+def test_fron_insertion():
+    None
+
+
+def test_endinsertion():
+    None
+
+
+def test_view_clean_insert():
+    None
+
+
+def test_view_after_insert():
+    None
+
+
+def test_view_move():
+    None

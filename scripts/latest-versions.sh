9f54490d1dc481a31f38226099972385a2940c98
Matt <1009003+tantaman@users.noreply.github.com>
2023-08-23 12:06:15 -0400

    remove scripts that belong in js repo

--- scripts/latest-versions.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
-source "$SCRIPT_DIR/pkgs.inc.sh"
-
-printf '%s\n' "${pkgs[@]}" | xargs -I {} -P 8 bash -c 'echo "{}:$(npm show {} versions --json | jq -r '.[-1]')"'

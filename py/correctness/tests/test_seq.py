0d16b1f9d80e8905253d9a8ae31c8cc12cb7e216
Matt <1009003+tantaman@users.noreply.github.com>
2023-08-28 11:52:31 -0400

    test "resurrection" of a row gets the correct seq value

--- py/correctness/tests/test_seq.py
+++ py/correctness/tests/test_seq.py
@@ -154,6 +154,97 @@ def test_incr_by_one():
     # pprint(c.execute(
     #     "SELECT *, seq FROM crsql_changes WHERE [table] = 'baz'").fetchall())
 
+
+# Test seq when resurrecting.
+# When a row is deleted and then resurrected through a future insertion with the same primary key
+# we have special logic for handling the seq value.
+# This tests to ensure seq values are still assigned as expected.
+def test_seq_when_re_inserting():
+    c = connect(":memory:")
+    c.execute("create table foo (a primary key, b)")
+    c.execute("select crsql_as_crr('foo')")
+    c.commit()
+
+    c.execute("INSERT INTO foo VALUES (1, 2)")
+    c.commit()
+    seq = c.execute("SELECT seq FROM crsql_changes").fetchone()[0]
+    assert (seq == 0)
+
+    c.execute("DELETE FROM foo")
+    c.commit()
+    seq = c.execute("SELECT seq FROM crsql_changes").fetchone()[0]
+    assert (seq == 0)
+
+    # re-insert a row with the same primary key as the deleted one
+    c.execute("INSERT INTO foo VALUES (1, 2)")
+    c.commit()
+    seqs = c.execute("SELECT seq FROM crsql_changes").fetchall()
+    # Seqs are appropriately assigned
+    # 0 for resurrect
+    # 1 for setting b to 2
+    assert (seqs == [(0,), (1,)])
+
+
+# Similar to the above test (test_seq_when_re_inserting) but also performs some updates
+# against the row that was re-inserted
+def test_seq_when_updating_after_reinsert():
+    c = connect(":memory:")
+    c.execute("create table foo (a primary key, b)")
+    c.execute("select crsql_as_crr('foo')")
+    c.commit()
+
+    c.execute("INSERT INTO foo VALUES (1, 2)")
+    c.commit()
+
+    c.execute("DELETE FROM foo")
+    c.commit()
+
+    # re-insert a row with the same primary key as the deleted one
+    c.execute("INSERT INTO foo VALUES (1, 2)")
+    c.execute("UPDATE foo SET b = 3 WHERE a = 1")
+    seqs = c.execute("SELECT seq FROM crsql_changes").fetchall()
+    c.commit()
+    # 0 for resurrect
+    # 2 for set b = 3 given 1 was used for b = 2 on the insert
+    assert (seqs == [(0,), (2,)])
+
+
+# Same as above tests but re-insertion is done via a merge with another node
+# rather than a local insert.
+def test_seq_when_resinserting_from_merge():
+    c = connect(":memory:")
+    c.execute("create table foo (a primary key, b)")
+    c.execute("select crsql_as_crr('foo')")
+    c.commit()
+
+    c.execute("INSERT INTO foo VALUES (1, 2)")
+    c.commit()
+    c.execute("DELETE FROM foo")
+    c.commit()
+
+    c.execute("INSERT INTO foo VALUES (1, 2)")
+    c.commit()
+
+    c2 = connect(":memory:")
+    c2.execute("create table foo (a primary key, b)")
+    c2.execute("select crsql_as_crr('foo')")
+    c2.commit()
+
+    c2.execute("INSERT INTO foo VALUES (1, 2)")
+    c2.commit()
+    c2.execute("DELETE FROM foo")
+    c2.commit()
+
+    # merge the resurrect over
+    sync_left_to_right(c, c2)
+
+    seqs = c2.execute("SELECT seq FROM crsql_changes").fetchall()
+    # Seqs are appropriately assigned
+    # 0 for resurrect
+    # 1 for setting b to 2
+    assert (seqs == [(0,), (1,)])
+
+
 # db is locked when doing concurrent writes so the following case cannot happen in SQLite
 # def test_preserved_conc_transactions():
 #     filename = "./test_preserved_conc_transactions.db"

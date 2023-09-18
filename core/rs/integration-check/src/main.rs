ed29ab0f1dbda3f2866e8af69be586155d01cf34
Matt <1009003+tantaman@users.noreply.github.com>
2023-09-18 10:09:00 -0400

    attempt to keep symbols by faking a call to `core_init`

--- core/rs/integration-check/src/main.rs
+++ core/rs/integration-check/src/main.rs
@@ -1,12 +1,19 @@
+use std::env;
 use std::ffi::c_void;
+use std::ptr;
 
 extern crate crsql_bundle;
 
-// Force the linker to keep this stuff even though it is not used in our crate
-pub use crsql_bundle::crsql_core::*;
-pub use crsql_bundle::sqlite::*;
-pub use crsql_bundle::*;
+extern "C" {
+    pub fn core_init(d: *mut c_char);
+}
 
 pub fn main() {
     println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    if args.len() > 5 {
+        unsafe {
+            core_init(ptr::null_mut());
+        }
+    }
 }

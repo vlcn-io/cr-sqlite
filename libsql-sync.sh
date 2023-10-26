92903f465b5f437cef623e79e09a3e0f1299a430
Matt <1009003+tantaman@users.noreply.github.com>
2023-10-26 14:03:21 -0400

    update libsql sync to new sqlite directory

--- libsql-sync.sh
+++ libsql-sync.sh
@@ -1,1 +1,1 @@
-rsync -vhra ./core/ ../libsql/ext/crr/ --include='**.gitignore' --exclude='**.git' --filter=':- .gitignore' --delete-after
+rsync -vhra ./core/ ../libsql/libsql-sqlite3/ext/crr/ --include='**.gitignore' --exclude='**.git' --filter=':- .gitignore' --delete-after

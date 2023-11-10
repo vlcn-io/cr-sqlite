e92a0ef48663f915c8ea2d0e5f7178fde441f117
Matt <1009003+tantaman@users.noreply.github.com>
2023-11-10 08:34:07 -0500

    add libsql flag to makefile

--- libsql-sync.sh
+++ libsql-sync.sh
@@ -1,6 +1,9 @@
 rsync -vhra ./core/ ../libsql/libsql-sqlite3/ext/crr/ \
   --include='**.gitignore' \
   --exclude='**.git' \
+  --exclude='shell.c' \
+  --exclude='sqlite3.c' \
+  --exclude='sqlite' \
   --exclude='sqlite3.h' \
   --exclude='sqlite3ext.h' \
   --filter=':- .gitignore' \

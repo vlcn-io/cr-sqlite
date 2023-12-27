df9c3b425a6eb0cdd7cbcb4d9a2b0930e2140fde
Jerome Gravel-Niquet <jeromegn@gmail.com>
2023-12-27 11:02:56 -0500

    only prevent launching a database with a version under 0.15

--- core/rs/core/src/sha.rs
+++ core/rs/core/src/sha.rs
@@ -1,2 +1,2 @@
 // The sha of the commit that this version of crsqlite was built from.
-pub const SHA: &'static str = "3a01980562615001d765eec61e3ed58147a93f93";
+pub const SHA: &'static str = "da2e95270d76818cd487029ad3786e9463fa341b";
